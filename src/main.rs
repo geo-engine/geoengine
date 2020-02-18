7e666beff7dce774493c27c3eedfbea99cb118b1
Michael Mattig <mattig@mathematik.uni-marburg.de>
2020-02-18 17:37:09 +0100

    use asynchronous RwLock

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use warp::Filter;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 use geoengine_operators::Operator;
 
-
 #[tokio::main]
 async fn main() {
-    let workflow_registry = Arc::new(Mutex::new(HashMap::<usize, Operator>::new()));
+    let workflow_registry = Arc::new(RwLock::new(HashMap::<usize, Operator>::new()));
     let workflow_registry = warp::any().map(move || Arc::clone(&workflow_registry));
 
     let register_workflow = warp::post()
@@ -24,15 +24,15 @@ async fn main() {
     warp::serve(register_workflow.or(load_workflow)).run(([127, 0, 0, 1], 3030)).await
 }
 
-async fn register_workflow(operator: Operator, workflow_registry: Arc<Mutex<HashMap::<usize, Operator>>>) -> Result<impl warp::Reply, warp::Rejection>{
-    let mut wr = workflow_registry.lock().unwrap(); //.await?
+async fn register_workflow(operator: Operator, workflow_registry: Arc<RwLock<HashMap::<usize, Operator>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut wr = workflow_registry.write().await;
     let id = wr.len();
     wr.insert(id, operator);
     Ok(id.to_string())
 }
 
-async fn load_workflow(id: usize, workflow_registry: Arc<Mutex<HashMap::<usize, Operator>>>) -> Result<impl warp::Reply, warp::Rejection> {
-    let wr = workflow_registry.lock().unwrap(); //.await?
+async fn load_workflow(id: usize, workflow_registry: Arc<RwLock<HashMap::<usize, Operator>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    let wr = workflow_registry.read().await;
     match wr.get(&id) {
         Some(operator) => Ok(serde_json::to_string(operator).unwrap()),
         None => Ok("no".into())
