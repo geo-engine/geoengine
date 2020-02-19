2558b543896843834587b8d196a046d11f3a7576
Michael Mattig <mattig@mathematik.uni-marburg.de>
2020-02-19 09:21:17 +0100

    Reply with json and appropriate status code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use warp::Filter;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
+use warp::reply::Reply;
 
 use geoengine_operators::Operator;
 
@@ -28,13 +29,13 @@ async fn register_workflow(operator: Operator, workflow_registry: Arc<RwLock<Has
     let mut wr = workflow_registry.write().await;
     let id = wr.len();
     wr.insert(id, operator);
-    Ok(id.to_string())
+    Ok(warp::reply::json(&id))
 }
 
 async fn load_workflow(id: usize, workflow_registry: Arc<RwLock<HashMap::<usize, Operator>>>) -> Result<impl warp::Reply, warp::Rejection> {
     let wr = workflow_registry.read().await;
     match wr.get(&id) {
-        Some(operator) => Ok(serde_json::to_string(operator).unwrap()),
-        None => Ok("no".into())
+        Some(operator) => Ok(warp::reply::json(&operator).into_response()),
+        None => Ok(warp::http::StatusCode::NOT_FOUND.into_response())
     }
 }
