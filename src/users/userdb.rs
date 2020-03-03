9116cb9e9c3dd30d1158fbbc30f3fe4a94b3d6af
Michael Mattig <mattig@mathematik.uni-marburg.de>
2020-03-03 14:58:09 +0100

    doc tests for userdb

--- src/users/userdb.rs
+++ src/users/userdb.rs
@@ -22,6 +22,24 @@ pub struct HashMapUserDB {
 }
 
 impl UserDB for HashMapUserDB {
+    /// Register a user
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use geoengine_services::users::user::{UserRegistration, UserInput};
+    /// use geoengine_services::users::userdb::{UserDB, HashMapUserDB};
+    ///
+    /// let mut user_db = HashMapUserDB::default();
+    ///
+    /// let user_registration = UserRegistration {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into(),
+    ///     real_name: "Foo Bar".into()
+    /// }.validated().unwrap();
+    ///
+    /// assert!(user_db.register(user_registration).is_ok());
+    /// ```
     fn register(&mut self, user_registration: Validated<UserRegistration>) -> Result<UserIdentification> {
         let user_registration = user_registration.user_input;
         ensure!(
@@ -35,6 +53,31 @@ impl UserDB for HashMapUserDB {
         Ok(id)
     }
 
+    /// Log user in
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use geoengine_services::users::user::{UserRegistration, UserInput, UserCredentials};
+    /// use geoengine_services::users::userdb::{UserDB, HashMapUserDB};
+    ///
+    /// let mut user_db = HashMapUserDB::default();
+    ///
+    /// let user_registration = UserRegistration {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into(),
+    ///     real_name: "Foo Bar".into()
+    /// }.validated().unwrap();
+    ///
+    /// assert!(user_db.register(user_registration).is_ok());
+    ///
+    /// let user_credentials = UserCredentials {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into()
+    /// };
+    ///
+    /// assert!(user_db.login(user_credentials).is_ok());
+    /// ```
     fn login(&mut self, user_credentials: UserCredentials) -> Result<Session> {
         match self.users.get(&user_credentials.email) {
             Some(user) => {
@@ -50,6 +93,33 @@ impl UserDB for HashMapUserDB {
         }
     }
 
+    /// Log user out
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use geoengine_services::users::user::{UserRegistration, UserInput, UserCredentials};
+    /// use geoengine_services::users::userdb::{UserDB, HashMapUserDB};
+    ///
+    /// let mut user_db = HashMapUserDB::default();
+    ///
+    /// let user_registration = UserRegistration {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into(),
+    ///     real_name: "Foo Bar".into()
+    /// }.validated().unwrap();
+    ///
+    /// assert!(user_db.register(user_registration).is_ok());
+    ///
+    /// let user_credentials = UserCredentials {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into()
+    /// };
+    ///
+    /// let session = user_db.login(user_credentials).unwrap();
+    ///
+    /// assert!(user_db.logout(session.token).is_ok());
+    /// ```
     fn logout(&mut self, token: SessionToken) -> Result<()> {
         match self.sessions.remove(&token) {
             Some(_) => Ok(()),
@@ -57,6 +127,33 @@ impl UserDB for HashMapUserDB {
         }
     }
 
+    /// Get session for token
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use geoengine_services::users::user::{UserRegistration, UserInput, UserCredentials};
+    /// use geoengine_services::users::userdb::{UserDB, HashMapUserDB};
+    ///
+    /// let mut user_db = HashMapUserDB::default();
+    ///
+    /// let user_registration = UserRegistration {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into(),
+    ///     real_name: "Foo Bar".into()
+    /// }.validated().unwrap();
+    ///
+    /// assert!(user_db.register(user_registration).is_ok());
+    ///
+    /// let user_credentials = UserCredentials {
+    ///     email: "foo@bar.de".into(),
+    ///     password: "secret123".into()
+    /// };
+    ///
+    /// let session = user_db.login(user_credentials).unwrap();
+    ///
+    /// assert!(user_db.session(session.token).is_ok());
+    /// ```
     fn session(&self, token: SessionToken) -> Result<Session> {
         match self.sessions.get(&token) {
             Some(session) => Ok(session.clone()),
