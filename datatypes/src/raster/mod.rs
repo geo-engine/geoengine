225ebefd610595d3de9eae65b93dbfdfc08b22c8
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2020-08-26 11:45:49 +0200

    Macros on generic rasters

--- datatypes/src/raster/mod.rs
+++ datatypes/src/raster/mod.rs
@@ -5,6 +5,7 @@ mod grid_dimension;
 mod operations;
 mod raster_tile;
 mod typed_raster;
+
 pub use self::base_raster::{BaseRaster, Raster2D, Raster3D};
 pub use self::data_type::{
     DynamicRasterDataType, FromPrimitive, Pixel, RasterDataType, StaticRasterDataType, TypedValue,
@@ -43,8 +44,8 @@ where
     /// # Examples
     ///
     /// ```
-    /// use geoengine_datatypes::raster::{Raster, Dim, Raster2D, GridPixelAccess};
-    /// use geoengine_datatypes::primitives::TimeInterval;
+    /// use $crate::raster::{Raster, Dim, Raster2D, GridPixelAccess};
+    /// use $crate::primitives::TimeInterval;
     ///
     /// let mut raster2d = Raster2D::new(
     ///    [3, 2].into(),
@@ -73,8 +74,8 @@ where
     /// # Examples
     ///
     /// ```
-    /// use geoengine_datatypes::raster::{Raster, Dim, Raster2D, GridPixelAccessMut};
-    /// use geoengine_datatypes::primitives::TimeInterval;
+    /// use $crate::raster::{Raster, Dim, Raster2D, GridPixelAccessMut};
+    /// use $crate::primitives::TimeInterval;
     ///
     /// let mut raster2d = Raster2D::new(
     ///    [3, 2].into(),
@@ -110,28 +111,190 @@ impl<T> Capacity for [T] {
         self.len()
     }
 }
+
 impl<T> Capacity for &[T] {
     fn capacity(&self) -> usize {
         self.len()
     }
 }
+
 impl<T> Capacity for [T; 1] {
     fn capacity(&self) -> usize {
         self.len()
     }
 }
+
 impl<T> Capacity for [T; 2] {
     fn capacity(&self) -> usize {
         self.len()
     }
 }
+
 impl<T> Capacity for [T; 3] {
     fn capacity(&self) -> usize {
         self.len()
     }
 }
+
 impl<T> Capacity for Vec<T> {
     fn capacity(&self) -> usize {
         self.len()
+    }
+}
+
+/// Maps a `TypedRaster2D` to another `TypedRaster2D` by calling a function on all its variants.
+/// Call via `map_generic_raster2d!(input, raster => function)`.
+#[macro_export]
+macro_rules! map_generic_raster2d {
+    ($input_raster:expr, $raster:ident => $function_call:expr) => {
+        map_generic_raster2d!(
+            @variants $input_raster, $raster => $function_call,
+            U8, U16, U32, U64, I8, I16, I32, I64, F32, F64
+        )
+    };
+
+    (@variants $input_raster:expr, $raster:ident => $function_call:expr, $($variant:tt),+) => {
+        match $input_raster {
+            $(
+                $crate::raster::TypedRaster2D::$variant($raster) => {
+                    $crate::raster::TypedRaster2D::$variant($function_call)
+                }
+            )+
+        }
+    };
+}
+
+/// Calls a function on a `TypedRaster2D`  by calling it on all its variants.
+/// Call via `call_generic_raster2d!(input, raster => function)`.
+#[macro_export]
+macro_rules! call_generic_raster2d {
+    ($input_raster:expr, $raster:ident => $function_call:expr) => {
+        call_generic_raster2d!(
+            @variants $input_raster, $raster => $function_call,
+            U8, U16, U32, U64, I8, I16, I32, I64, F32, F64
+        )
+    };
+
+    (@variants $input_raster:expr, $raster:ident => $function_call:expr, $($variant:tt),+) => {
+        match $input_raster {
+            $(
+                $crate::raster::TypedRaster2D::$variant($raster) => $function_call,
+            )+
+        }
+    };
+}
+
+/// Generates a a `TypedRaster2D` by calling a function for all variants.
+/// Call via `generate_generic_raster2d!(type, function)`.
+#[macro_export]
+macro_rules! generate_generic_raster2d {
+    ($type_enum:expr, $function_call:expr) => {
+        generate_generic_raster2d!(
+            @variants $type_enum, $function_call,
+            U8, U16, U32, U64, I8, I16, I32, I64, F32, F64
+        )
+    };
+
+    (@variants $type_enum:expr, $function_call:expr, $($variant:tt),+) => {
+        match $type_enum {
+            $(
+                $crate::raster::RasterDataType::$variant => {
+                    crate::raster::TypedRaster2D::$variant($function_call)
+                }
+            )+
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn map_generic_raster2d() {
+        fn map<T: Pixel>(raster: Raster2D<T>) -> Raster2D<T> {
+            raster
+        }
+
+        let typed_raster = TypedRaster2D::U32(
+            Raster2D::new(
+                [3, 2].into(),
+                vec![1, 2, 3, 4, 5, 6],
+                None,
+                Default::default(),
+                [1.0, 1.0, 0.0, 1.0, 0.0, 1.0].into(),
+            )
+            .unwrap(),
+        );
+
+        assert_eq!(
+            typed_raster,
+            map_generic_raster2d!(typed_raster.clone(), raster => map(raster))
+        );
+    }
+
+    #[test]
+    fn call_generic_raster2d() {
+        fn first_pixel<T: Pixel>(raster: &Raster2D<T>) -> i64 {
+            raster.pixel_value_at_grid_index(&(0, 0)).unwrap().as_()
+        }
+
+        let typed_raster = TypedRaster2D::U32(
+            Raster2D::new(
+                [3, 2].into(),
+                vec![1, 2, 3, 4, 5, 6],
+                None,
+                Default::default(),
+                [1.0, 1.0, 0.0, 1.0, 0.0, 1.0].into(),
+            )
+            .unwrap(),
+        );
+
+        assert_eq!(
+            call_generic_raster2d!(typed_raster.clone(), _raster => 2),
+            2
+        );
+
+        assert_eq!(
+            call_generic_raster2d!(typed_raster, raster => first_pixel(&raster)),
+            1
+        );
+    }
+
+    #[test]
+    fn generate_generic_raster2d() {
+        fn generate<T: Pixel>() -> Raster2D<T> {
+            let data: Vec<T> = vec![
+                T::from_(1),
+                T::from_(2),
+                T::from_(3),
+                T::from_(4),
+                T::from_(5),
+                T::from_(6),
+            ];
+
+            Raster2D::new(
+                [3, 2].into(),
+                data,
+                None,
+                Default::default(),
+                [1.0, 1.0, 0.0, 1.0, 0.0, 1.0].into(),
+            )
+            .unwrap()
+        }
+
+        assert_eq!(
+            generate_generic_raster2d!(RasterDataType::U8, generate()),
+            TypedRaster2D::U8(
+                Raster2D::new(
+                    [3, 2].into(),
+                    vec![1, 2, 3, 4, 5, 6],
+                    None,
+                    Default::default(),
+                    [1.0, 1.0, 0.0, 1.0, 0.0, 1.0].into(),
+                )
+                .unwrap(),
+            )
+        );
     }
 }
