8ce1d79fbecfae2bb1181afda9d4620a2e7a2503
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2020-08-26 17:31:24 +0200

    extracted `ResultDescriptor` map functions to trait

--- operators/src/engine/result_descriptor.rs
+++ operators/src/engine/result_descriptor.rs
@@ -3,10 +3,29 @@ use geoengine_datatypes::{
 };
 use serde::{Deserialize, Serialize};
 
+/// A descriptor that contains information about the query result, for instance, the data type
+/// and projection.
 pub trait ResultDescriptor: Copy {
     type DataType;
+
     fn data_type(&self) -> Self::DataType;
+
     fn projection(&self) -> ProjectionOption;
+
+    fn map<F>(self, f: F) -> Self
+    where
+        F: Fn(Self) -> Self,
+    {
+        f(self)
+    }
+
+    fn map_projection<F>(self, f: F) -> Self
+    where
+        F: Fn(Self::DataType) -> Self::DataType;
+
+    fn map_data_type<F>(self, f: F) -> Self
+    where
+        F: Fn(ProjectionOption) -> ProjectionOption;
 }
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Serialize, Deserialize)]
@@ -15,38 +34,31 @@ pub struct RasterResultDescriptor {
     pub projection: ProjectionOption,
 }
 
-impl RasterResultDescriptor {
-    pub fn map<F>(self, f: F) -> Self
-    where
-        F: Fn(Self) -> Self,
-    {
-        f(self)
+impl ResultDescriptor for RasterResultDescriptor {
+    type DataType = RasterDataType;
+
+    fn data_type(&self) -> Self::DataType {
+        self.data_type
     }
 
-    pub fn map_data_type<F>(mut self, f: F) -> Self
+    fn projection(&self) -> ProjectionOption {
+        self.projection
+    }
+
+    fn map_projection<F>(mut self, f: F) -> Self
     where
-        F: Fn(RasterDataType) -> RasterDataType,
+        F: Fn(Self::DataType) -> Self::DataType,
     {
         self.data_type = f(self.data_type);
         self
     }
 
-    pub fn map_projection<F>(mut self, f: F) -> Self
+    fn map_data_type<F>(mut self, f: F) -> Self
     where
         F: Fn(ProjectionOption) -> ProjectionOption,
     {
         self.projection = f(self.projection);
         self
-    }
-}
-
-impl ResultDescriptor for RasterResultDescriptor {
-    type DataType = RasterDataType;
-    fn data_type(&self) -> Self::DataType {
-        self.data_type
-    }
-    fn projection(&self) -> ProjectionOption {
-        self.projection
     }
 }
 
@@ -63,30 +75,32 @@ impl VectorResultDescriptor {
     {
         f(self)
     }
+}
 
-    pub fn map_data_type<F>(mut self, f: F) -> Self
+impl ResultDescriptor for VectorResultDescriptor {
+    type DataType = VectorDataType;
+
+    fn data_type(&self) -> Self::DataType {
+        self.data_type
+    }
+
+    fn projection(&self) -> ProjectionOption {
+        self.projection
+    }
+
+    fn map_projection<F>(mut self, f: F) -> Self
     where
-        F: Fn(VectorDataType) -> VectorDataType,
+        F: Fn(Self::DataType) -> Self::DataType,
     {
         self.data_type = f(self.data_type);
         self
     }
 
-    pub fn map_projection<F>(mut self, f: F) -> Self
+    fn map_data_type<F>(mut self, f: F) -> Self
     where
         F: Fn(ProjectionOption) -> ProjectionOption,
     {
         self.projection = f(self.projection);
         self
-    }
-}
-
-impl ResultDescriptor for VectorResultDescriptor {
-    type DataType = VectorDataType;
-    fn data_type(&self) -> Self::DataType {
-        self.data_type
-    }
-    fn projection(&self) -> ProjectionOption {
-        self.projection
     }
 }
