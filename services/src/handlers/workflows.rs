f83be3574f2b1e65712a3c971d086414445eab08
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2021-02-03 15:13:53 +0100

    method for querying workflow result descriptors

--- services/src/handlers/workflows.rs
+++ services/src/handlers/workflows.rs
@@ -2,10 +2,13 @@ use uuid::Uuid;
 use warp::reply::Reply;
 use warp::Filter;
 
+use crate::error;
 use crate::handlers::{authenticate, Context};
 use crate::util::IdResponse;
 use crate::workflows::registry::WorkflowRegistry;
 use crate::workflows::workflow::{Workflow, WorkflowId};
+use geoengine_operators::engine::MockExecutionContext;
+use snafu::ResultExt;
 
 pub(crate) fn register_workflow_handler<C: Context>(
     ctx: C,
@@ -49,6 +52,37 @@ async fn load_workflow<C: Context>(id: Uuid, ctx: C) -> Result<impl warp::Reply,
     Ok(warp::reply::json(&wf).into_response())
 }
 
+pub(crate) fn get_workflow_metadata_handler<C: Context>(
+    ctx: C,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+    warp::get()
+        .and(warp::path!("workflow" / "metadata" / Uuid))
+        .and(authenticate(ctx))
+        .and_then(get_workflow_metadata)
+}
+
+// TODO: move into handler once async closures are available?
+async fn get_workflow_metadata<C: Context>(
+    id: Uuid,
+    ctx: C,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    // TODO: check for rights to access workflow?
+    let workflow = ctx
+        .workflow_registry_ref()
+        .await
+        .load(&WorkflowId(id))
+        .await?;
+    let operator = workflow.operator.get_vector().context(error::Operator)?;
+    // TODO: use meaningful context to initialize
+    let operator = operator
+        .initialize(&MockExecutionContext::default())
+        .context(error::Operator)?;
+
+    let result_descriptor = operator.result_descriptor();
+
+    Ok(warp::reply::json(result_descriptor))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,8 +91,12 @@ mod tests {
     use crate::util::user_input::UserInput;
     use crate::util::IdResponse;
     use crate::{contexts::InMemoryContext, workflows::registry::WorkflowRegistry};
+    use geoengine_datatypes::collections::MultiPointCollection;
+    use geoengine_datatypes::primitives::{FeatureData, MultiPoint, TimeInterval};
     use geoengine_operators::engine::VectorOperator;
-    use geoengine_operators::mock::{MockPointSource, MockPointSourceParams};
+    use geoengine_operators::mock::{
+        MockFeatureCollectionSource, MockPointSource, MockPointSourceParams,
+    };
 
     #[tokio::test]
     async fn register() {
@@ -192,5 +230,88 @@ mod tests {
             .await;
 
         assert_eq!(res.status(), 404);
+    }
+
+    #[tokio::test]
+    async fn metadata() {
+        let ctx = InMemoryContext::default();
+
+        ctx.user_db()
+            .write()
+            .await
+            .register(
+                UserRegistration {
+                    email: "foo@bar.de".to_string(),
+                    password: "secret123".to_string(),
+                    real_name: "Foo Bar".to_string(),
+                }
+                .validated()
+                .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        let session = ctx
+            .user_db()
+            .write()
+            .await
+            .login(UserCredentials {
+                email: "foo@bar.de".to_string(),
+                password: "secret123".to_string(),
+            })
+            .await
+            .unwrap();
+
+        let workflow = Workflow {
+            operator: MockFeatureCollectionSource::single(
+                MultiPointCollection::from_data(
+                    MultiPoint::many(vec![(0.0, 0.1)]).unwrap(),
+                    vec![TimeInterval::default()],
+                    [
+                        ("foo".to_string(), FeatureData::Number(vec![42.0])),
+                        ("bar".to_string(), FeatureData::Decimal(vec![23])),
+                    ]
+                    .iter()
+                    .cloned()
+                    .collect(),
+                )
+                .unwrap(),
+            )
+            .boxed()
+            .into(),
+        };
+
+        let id = ctx
+            .workflow_registry()
+            .write()
+            .await
+            .register(workflow.clone())
+            .await
+            .unwrap();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path(&format!("/workflow/metadata/{}", id.to_string()))
+            .header(
+                "Authorization",
+                format!("Bearer {}", session.id.to_string()),
+            )
+            .reply(&get_workflow_metadata_handler(ctx))
+            .await;
+
+        assert_eq!(res.status(), 200);
+
+        assert_eq!(
+            *res.body(),
+            serde_json::json!({
+                "data_type": "MultiPoint",
+                "spatial_reference": "EPSG:4326",
+                "columns": {
+                    "bar": "Decimal",
+                    "foo": "Number"
+                }
+            })
+            .to_string()
+        );
     }
 }
