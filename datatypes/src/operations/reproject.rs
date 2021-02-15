3ca331dbd02c25630046066415d03bb3eef411b7
Johannes Drönner <droenner@informatik.uni-marburg.de>
2021-02-15 17:08:36 +0100

    fix tests and simplify trait

--- datatypes/src/operations/reproject.rs
+++ datatypes/src/operations/reproject.rs
@@ -12,8 +12,9 @@ use crate::{
 };
 
 pub trait CoordinateProjector {
-    type Projector: CoordinateProjector;
-    fn from_known_srs(from: SpatialReference, to: SpatialReference) -> Result<Self::Projector>;
+    fn from_known_srs(from: SpatialReference, to: SpatialReference) -> Result<Self>
+    where
+        Self: Sized;
     fn project_coordinate(&self, c: Coordinate2D) -> Result<Coordinate2D>;
 
     // TODO: add for performance
@@ -23,11 +24,7 @@ pub trait CoordinateProjector {
 
 // TODO: move Proj impl into a separate module?
 impl CoordinateProjector for Proj {
-    type Projector = Proj;
-
     fn from_known_srs(from: SpatialReference, to: SpatialReference) -> Result<Self> {
-        dbg!(from, to);
-
         Proj::new_known_crs(&from.to_string(), &to.to_string(), None)
             .ok_or(error::Error::NoCoordinateProjector { from, to })
     }
@@ -157,15 +154,15 @@ where
 {
     type Out = BoundingBox2D;
     fn reproject(&self, projector: &P) -> Result<BoundingBox2D> {
-        let points_per_line = 7; // TODO: static or something else ?
+        const POINTS_PER_LINE: i32 = 7;
         let upper_line = Line::new(self.upper_left(), self.upper_right())
-            .with_additional_equi_spaced_coords(points_per_line);
+            .with_additional_equi_spaced_coords(POINTS_PER_LINE);
         let right_line = Line::new(self.upper_right(), self.lower_right())
-            .with_additional_equi_spaced_coords(points_per_line);
+            .with_additional_equi_spaced_coords(POINTS_PER_LINE);
         let lower_line = Line::new(self.lower_right(), self.lower_left())
-            .with_additional_equi_spaced_coords(points_per_line);
+            .with_additional_equi_spaced_coords(POINTS_PER_LINE);
         let left_line = Line::new(self.lower_left(), self.upper_left())
-            .with_additional_equi_spaced_coords(points_per_line);
+            .with_additional_equi_spaced_coords(POINTS_PER_LINE);
 
         let cs: Vec<Coordinate2D> = upper_line
             .chain(right_line)
@@ -186,14 +183,25 @@ mod tests {
 
     use super::*;
 
+    // coordinates used for the tests in EPSG:4326
+    // and reprojected with proj cs2cs to EPSG:900913
+    //
+    // cs2cs -d 10  EPSG:4326 EPSG:900913
+    // 50.8021728 8.7667933
+    // 975914.9660458824       6586374.7028446598 0.0000000000
+    // 50.937531 6.9602786
+    // 774814.6695313191       6610251.1099264193 0.0000000000
+    // 53.565278 10.001389
+    // 1113349.5307054475      7088251.2962248782 0.0000000000
+
     const MARBURG_EPSG_4326: Coordinate2D = Coordinate2D {
         x: 8.766_793_3,
         y: 50.802_172_8,
     };
 
     const MARBURG_EPSG_900_913: Coordinate2D = Coordinate2D {
-        x: 975_914.969_756_16,
-        y: 6_586_374.708_710_07,
+        x: 975_914.966_045_882,
+        y: 6_586_374.702_844_659,
     };
 
     const COLOGNE_EPSG_4326: Coordinate2D = Coordinate2D {
@@ -202,8 +210,8 @@ mod tests {
     };
 
     const COLOGNE_EPSG_900_913: Coordinate2D = Coordinate2D {
-        x: 774_814.669_531_32,
-        y: 6_610_251.109_926_42,
+        x: 774_814.669_531_319,
+        y: 6_610_251.109_926_419,
     };
 
     const HAMBURG_EPSG_4326: Coordinate2D = Coordinate2D {
@@ -212,8 +220,8 @@ mod tests {
     };
 
     const HAMBURG_EPSG_900_913: Coordinate2D = Coordinate2D {
-        x: 1_113_349.518_337_85,
-        y: 7_088_251.306_646_32,
+        x: 1_113_349.530_705_447,
+        y: 7_088_251.296_224_878,
     };
 
     #[test]
@@ -239,8 +247,8 @@ mod tests {
         let p = Proj::from_known_srs(from, to).unwrap();
         let rp = p.project_coordinate(MARBURG_EPSG_4326).unwrap();
 
-        approx_eq!(f64, rp.x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.y, MARBURG_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rp.x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.y, MARBURG_EPSG_900_913.y));
     }
 
     #[test]
@@ -250,8 +258,8 @@ mod tests {
         let p = Proj::from_known_srs(from, to).unwrap();
         let rp = MARBURG_EPSG_4326.reproject(&p).unwrap();
 
-        approx_eq!(f64, rp.x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.y, MARBURG_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rp.x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.y, MARBURG_EPSG_900_913.y));
     }
 
     #[test]
@@ -266,10 +274,10 @@ mod tests {
         };
         let rl = l.reproject(&p).unwrap();
 
-        approx_eq!(f64, rl.start.x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rl.start.y, MARBURG_EPSG_900_913.y);
-        approx_eq!(f64, rl.end.x, COLOGNE_EPSG_900_913.x);
-        approx_eq!(f64, rl.end.y, COLOGNE_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rl.start.x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rl.start.y, MARBURG_EPSG_900_913.y));
+        assert!(approx_eq!(f64, rl.end.x, COLOGNE_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rl.end.y, COLOGNE_EPSG_900_913.y));
     }
 
     #[test]
@@ -283,10 +291,10 @@ mod tests {
 
         let rl = bbox.reproject(&p).unwrap();
 
-        approx_eq!(f64, rl.lower_left().x, COLOGNE_EPSG_900_913.x);
-        approx_eq!(f64, rl.lower_left().y, MARBURG_EPSG_900_913.y);
-        approx_eq!(f64, rl.upper_right().x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rl.upper_right().y, COLOGNE_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rl.lower_left().x, COLOGNE_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rl.lower_left().y, MARBURG_EPSG_900_913.y));
+        assert!(approx_eq!(f64, rl.upper_right().x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rl.upper_right().y, COLOGNE_EPSG_900_913.y));
     }
 
     #[test]
@@ -300,10 +308,10 @@ mod tests {
         let mp = MultiPoint::new(cs).unwrap();
         let rp = mp.reproject(&p).unwrap();
 
-        approx_eq!(f64, rp.points()[0].x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.points()[0].y, MARBURG_EPSG_900_913.y);
-        approx_eq!(f64, rp.points()[1].x, COLOGNE_EPSG_900_913.x);
-        approx_eq!(f64, rp.points()[1].y, COLOGNE_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rp.points()[0].x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.points()[0].y, MARBURG_EPSG_900_913.y));
+        assert!(approx_eq!(f64, rp.points()[1].x, COLOGNE_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.points()[1].y, COLOGNE_EPSG_900_913.y));
     }
 
     #[test]
@@ -321,12 +329,12 @@ mod tests {
         let mp = MultiLineString::new(cs).unwrap();
         let rp = mp.reproject(&p).unwrap();
 
-        approx_eq!(f64, rp.lines()[0][0].x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.lines()[0][0].y, MARBURG_EPSG_900_913.y);
-        approx_eq!(f64, rp.lines()[0][1].x, COLOGNE_EPSG_900_913.x);
-        approx_eq!(f64, rp.lines()[0][1].y, COLOGNE_EPSG_900_913.y);
-        approx_eq!(f64, rp.lines()[0][2].x, HAMBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.lines()[0][2].y, HAMBURG_EPSG_900_913.y);
+        assert!(approx_eq!(f64, rp.lines()[0][0].x, MARBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.lines()[0][0].y, MARBURG_EPSG_900_913.y));
+        assert!(approx_eq!(f64, rp.lines()[0][1].x, COLOGNE_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.lines()[0][1].y, COLOGNE_EPSG_900_913.y));
+        assert!(approx_eq!(f64, rp.lines()[0][2].x, HAMBURG_EPSG_900_913.x));
+        assert!(approx_eq!(f64, rp.lines()[0][2].y, HAMBURG_EPSG_900_913.y));
     }
 
     #[test]
@@ -344,12 +352,37 @@ mod tests {
 
         let mp = MultiPolygon::new(cs).unwrap();
         let rp = mp.reproject(&p).unwrap();
+        dbg!(&mp, &rp);
 
-        approx_eq!(f64, rp.polygons()[0][0][0].x, MARBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.polygons()[0][0][0].y, MARBURG_EPSG_900_913.y);
-        approx_eq!(f64, rp.polygons()[0][0][1].x, COLOGNE_EPSG_900_913.x);
-        approx_eq!(f64, rp.polygons()[0][0][1].y, COLOGNE_EPSG_900_913.y);
-        approx_eq!(f64, rp.polygons()[0][0][2].x, HAMBURG_EPSG_900_913.x);
-        approx_eq!(f64, rp.polygons()[0][0][2].y, HAMBURG_EPSG_900_913.y);
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][0].x,
+            MARBURG_EPSG_900_913.x
+        ));
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][0].y,
+            MARBURG_EPSG_900_913.y
+        ));
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][1].x,
+            COLOGNE_EPSG_900_913.x
+        ));
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][1].y,
+            COLOGNE_EPSG_900_913.y
+        ));
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][2].x,
+            HAMBURG_EPSG_900_913.x
+        ));
+        assert!(approx_eq!(
+            f64,
+            rp.polygons()[0][0][2].y,
+            HAMBURG_EPSG_900_913.y
+        ));
     }
 }
