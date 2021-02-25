5c671e69bab9927c65419070cd240691371ab76c
Michael Mattig <mattig@mathematik.uni-marburg.de>
2021-02-25 13:44:10 +0100

    fix import

--- operators/src/source/gdal_source.rs
+++ operators/src/source/gdal_source.rs
@@ -518,7 +518,7 @@ mod tests {
     use crate::engine::{MockExecutionContext, MockQueryContext, QueryRectangle};
     use crate::util::gdal::{add_ndvi_data_set, raster_dir};
     use crate::util::Result;
-    use geoengine_datatypes::primitives::{Measurement, SpatialResolution, TimeGranularity};
+    use geoengine_datatypes::{primitives::{Measurement, SpatialResolution, TimeGranularity}, raster::GridShape2D};
     use geoengine_datatypes::raster::{TileInformation, TilingStrategy};
     use geoengine_datatypes::{raster::GridIdx2D, spatial_reference::SpatialReference};
 
