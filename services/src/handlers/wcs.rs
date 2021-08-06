03f52416cf9d0f91d7ddba30286b829cdaaf88ec
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2021-08-06 22:29:41 +0200

    fmt

--- services/src/handlers/wcs.rs
+++ services/src/handlers/wcs.rs
@@ -616,8 +616,9 @@ mod tests {
 
         assert_eq!(res.status(), 200);
         assert_eq!(
-            include_bytes!("../../../operators/test-data/raster/geotiff_from_stream_compressed.tiff")
-                as &[u8],
+            include_bytes!(
+                "../../../operators/test-data/raster/geotiff_from_stream_compressed.tiff"
+            ) as &[u8],
             res.body().to_vec().as_slice()
         );
     }
