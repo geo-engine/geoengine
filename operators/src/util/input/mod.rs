085bd1d890028d8a7793a4e0ce5f1f1382ca9178
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2021-10-07 17:33:01 +0200

    serde modules for floats with NaNs as strings

--- operators/src/util/input/mod.rs
+++ operators/src/util/input/mod.rs
@@ -1,7 +1,9 @@
+mod float_with_nan_serde;
 mod raster_or_vector;
 mod string_or_number;
 mod string_or_number_range;
 
+pub use float_with_nan_serde::{float as float_with_nan, float_option as float_option_with_nan};
 pub use raster_or_vector::RasterOrVectorOperator;
 pub use string_or_number::StringOrNumber;
 pub use string_or_number_range::StringOrNumberRange;
