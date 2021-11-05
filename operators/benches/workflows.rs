8a9619593a71faceeae42b196750e2f5877f9f9b
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2021-11-05 17:53:06 +0100

    add benches for projection

--- operators/benches/workflows.rs
+++ operators/benches/workflows.rs
@@ -6,14 +6,17 @@ use futures::StreamExt;
 use geoengine_datatypes::dataset::DatasetId;
 use geoengine_datatypes::primitives::Measurement;
 use geoengine_datatypes::raster::RasterDataType;
+use geoengine_datatypes::spatial_reference::SpatialReference;
 use geoengine_datatypes::util::Identifier;
 use geoengine_datatypes::{
     dataset::InternalDatasetId,
     primitives::{SpatialPartition2D, SpatialResolution, TimeInterval},
     raster::{GridSize, RasterTile2D, TilingSpecification},
 };
-use geoengine_operators::engine::MetaData;
-use geoengine_operators::processing::{Expression, ExpressionParams, ExpressionSources};
+use geoengine_operators::engine::{MetaData, SingleRasterOrVectorSource};
+use geoengine_operators::processing::{
+    Expression, ExpressionParams, ExpressionSources, Reprojection, ReprojectionParams,
+};
 use geoengine_operators::source::GdalSource;
 use geoengine_operators::{
     engine::{
@@ -63,10 +66,12 @@ fn bench_raster_operator<'a, Q, T, F, C>(
 
         for (qrect_name, qrect) in named_querys.clone().into_iter() {
             run_time.block_on(async {
-                let start = Instant::now();
-
+                let start_query = Instant::now();
                 // query the operator
                 let query = initialized_queryprocessor.raster_query(*qrect, ctx).await.unwrap();
+                let query_elapsed = start_query.elapsed();
+
+                let start = Instant::now();
                 // drain the stream
                 // TODO: build a custom drain that does not collect all the tiles
                 let res: Vec<Result<RasterTile2D<_>, _>> = query.collect().await;
@@ -77,11 +82,13 @@ fn bench_raster_operator<'a, Q, T, F, C>(
                 let number_of_tiles = black_box(res.into_iter().map(Result::unwrap).count());
 
                 println!(
-                    "Bench \"{}\" | tile size \"[{}, {}]\" | query \"{}\" | produced {} tiles ({} px) | in {} s  ({} ns)",
+                    "Bench \"{}\" | tile size \"[{}, {}]\" | query \"{}\" in {} s ({} ns) | produced {} tiles ({} px) | in {} s  ({} ns)",
                     bench_id,
                     tiling_spec.tile_size_in_pixels.axis_size_y(),
                     tiling_spec.tile_size_in_pixels.axis_size_x(),
                     qrect_name,
+                    query_elapsed.as_secs_f32(),
+                    query_elapsed.as_nanos(),
                     number_of_tiles,
                     number_of_tiles as u128 * tiling_spec.tile_size_in_pixels.number_of_elements() as u128,
                     elapsed.as_secs_f32(),
@@ -160,7 +167,7 @@ fn bench_gdal_source_operator_with_expression_tile_size() {
     let ctx = MockQueryContext::with_chunk_size_and_thread_count(ChunkByteSize::MAX, 8);
 
     let tiling_specs = vec![
-        TilingSpecification::new((0., 0.).into(), [32, 32].into()),
+        // TilingSpecification::new((0., 0.).into(), [32, 32].into()),
         TilingSpecification::new((0., 0.).into(), [64, 64].into()),
         TilingSpecification::new((0., 0.).into(), [128, 128].into()),
         TilingSpecification::new((0., 0.).into(), [256, 256].into()),
@@ -208,7 +215,133 @@ fn bench_gdal_source_operator_with_expression_tile_size() {
     );
 }
 
+fn bench_gdal_source_operator_with_identity_reprojection() {
+    let qrects = vec![(
+        "World in 36000x18000 pixels",
+        RasterQueryRectangle {
+            spatial_bounds: SpatialPartition2D::new((-180., 90.).into(), (180., -90.).into())
+                .unwrap(),
+            time_interval: TimeInterval::new(1_388_534_400_000, 1_388_534_400_000 + 1000).unwrap(),
+            spatial_resolution: SpatialResolution::new(0.01, 0.01).unwrap(),
+        },
+    )];
+
+    let run_time = tokio::runtime::Runtime::new().unwrap();
+    let ctx = MockQueryContext::with_chunk_size_and_thread_count(ChunkByteSize::MAX, 8);
+
+    let tiling_specs = vec![
+        // TilingSpecification::new((0., 0.).into(), [32, 32].into()),
+        // TilingSpecification::new((0., 0.).into(), [64, 64].into()),
+        // TilingSpecification::new((0., 0.).into(), [128, 128].into()),
+        TilingSpecification::new((0., 0.).into(), [256, 256].into()),
+        TilingSpecification::new((0., 0.).into(), [512, 512].into()),
+        TilingSpecification::new((0., 0.).into(), [600, 600].into()),
+        TilingSpecification::new((0., 0.).into(), [900, 900].into()),
+        TilingSpecification::new((0., 0.).into(), [1024, 1024].into()),
+        TilingSpecification::new((0., 0.).into(), [2048, 2048].into()),
+        TilingSpecification::new((0., 0.).into(), [4096, 4096].into()),
+        TilingSpecification::new((0., 0.).into(), [9000, 9000].into()),
+    ];
+
+    let id: DatasetId = InternalDatasetId::new().into();
+    let meta_data = create_ndvi_meta_data();
+
+    let gdal_operator = GdalSource {
+        params: GdalSourceParameters {
+            dataset: id.clone(),
+        },
+    };
+
+    let projection_operator = Reprojection {
+        params: ReprojectionParams {
+            target_spatial_reference: SpatialReference::epsg_4326(),
+        },
+        sources: SingleRasterOrVectorSource::from(gdal_operator.boxed()),
+    };
+
+    bench_raster_operator(
+        "projection identity",
+        &qrects,
+        &tiling_specs,
+        projection_operator.boxed(),
+        |ts| {
+            let mut mex = MockExecutionContext::new_with_tiling_spec(ts);
+            mex.add_meta_data(id.clone(), meta_data.box_clone());
+            mex
+        },
+        &ctx,
+        &run_time,
+    );
+}
+
+fn bench_gdal_source_operator_with_4326_to_3857_reprojection() {
+    let qrects = vec![(
+        "World in EPSG:3857 ~ 40000 x 20000 px",
+        RasterQueryRectangle {
+            spatial_bounds: SpatialPartition2D::new(
+                (-20_037_508.342_789_244, 20_048_966.104_014_594).into(),
+                (20_037_508.342_789_244, -20_048_966.104_014_594).into(),
+            )
+            .unwrap(),
+            time_interval: TimeInterval::new(1_388_534_400_000, 1_388_534_400_000 + 1000).unwrap(),
+            spatial_resolution: SpatialResolution::new(1050., 2100.).unwrap(),
+        },
+    )];
+
+    let run_time = tokio::runtime::Runtime::new().unwrap();
+    let ctx = MockQueryContext::with_chunk_size_and_thread_count(ChunkByteSize::MAX, 8);
+
+    let tiling_specs = vec![
+        // TilingSpecification::new((0., 0.).into(), [32, 32].into()),
+        // TilingSpecification::new((0., 0.).into(), [64, 64].into()),
+        // TilingSpecification::new((0., 0.).into(), [128, 128].into()),
+        // TilingSpecification::new((0., 0.).into(), [256, 256].into()),
+        TilingSpecification::new((0., 0.).into(), [512, 512].into()),
+        TilingSpecification::new((0., 0.).into(), [600, 600].into()),
+        TilingSpecification::new((0., 0.).into(), [900, 900].into()),
+        TilingSpecification::new((0., 0.).into(), [1024, 1024].into()),
+        TilingSpecification::new((0., 0.).into(), [2048, 2048].into()),
+        TilingSpecification::new((0., 0.).into(), [4096, 4096].into()),
+        TilingSpecification::new((0., 0.).into(), [9000, 9000].into()),
+    ];
+
+    let id: DatasetId = InternalDatasetId::new().into();
+    let meta_data = create_ndvi_meta_data();
+
+    let gdal_operator = GdalSource {
+        params: GdalSourceParameters {
+            dataset: id.clone(),
+        },
+    };
+
+    let projection_operator = Reprojection {
+        params: ReprojectionParams {
+            target_spatial_reference: SpatialReference::new(
+                geoengine_datatypes::spatial_reference::SpatialReferenceAuthority::Epsg,
+                3857,
+            ),
+        },
+        sources: SingleRasterOrVectorSource::from(gdal_operator.boxed()),
+    };
+
+    bench_raster_operator(
+        "projection 4326 -> 3857",
+        &qrects,
+        &tiling_specs,
+        projection_operator.boxed(),
+        |ts| {
+            let mut mex = MockExecutionContext::new_with_tiling_spec(ts);
+            mex.add_meta_data(id.clone(), meta_data.box_clone());
+            mex
+        },
+        &ctx,
+        &run_time,
+    );
+}
+
 fn main() {
     bench_gdal_source_operator_tile_size();
     bench_gdal_source_operator_with_expression_tile_size();
+    bench_gdal_source_operator_with_identity_reprojection();
+    bench_gdal_source_operator_with_4326_to_3857_reprojection();
 }
