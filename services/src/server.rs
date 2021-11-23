0ccc7391afd664c6e07b871716f86a20cafae67e
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2021-11-23 18:18:22 +0100

    fixing actix versioning clash

--- services/src/server.rs
+++ services/src/server.rs
@@ -6,7 +6,7 @@ use crate::util::config;
 use crate::util::config::get_config_element;
 
 use actix_files::Files;
-use actix_web::dev::{Body, ServiceResponse};
+use actix_web::dev::{AnyBody, ServiceResponse};
 use actix_web::error::{InternalError, JsonPayloadError, QueryPayloadError};
 use actix_web::{http, middleware, web, App, HttpResponse, HttpServer};
 use log::{debug, info};
@@ -212,13 +212,13 @@ pub(crate) async fn show_version_handler() -> impl actix_web::Responder {
 #[allow(clippy::unnecessary_wraps)]
 pub(crate) fn render_404(
     mut res: ServiceResponse,
-) -> actix_web::Result<middleware::ErrorHandlerResponse<Body>> {
+) -> actix_web::Result<middleware::ErrorHandlerResponse<AnyBody>> {
     res.headers_mut().insert(
         http::header::CONTENT_TYPE,
         http::HeaderValue::from_static("application/json"),
     );
     let res = res.map_body(|_, _| {
-        Body::from(
+        AnyBody::from(
             serde_json::to_string(&ErrorResponse {
                 error: "NotFound".to_string(),
                 message: "Not Found".to_string(),
@@ -232,13 +232,13 @@ pub(crate) fn render_404(
 #[allow(clippy::unnecessary_wraps)]
 pub(crate) fn render_405(
     mut res: ServiceResponse,
-) -> actix_web::Result<middleware::ErrorHandlerResponse<Body>> {
+) -> actix_web::Result<middleware::ErrorHandlerResponse<AnyBody>> {
     res.headers_mut().insert(
         http::header::CONTENT_TYPE,
         http::HeaderValue::from_static("application/json"),
     );
     let res = res.map_body(|_, _| {
-        Body::from(
+        AnyBody::from(
             serde_json::to_string(&ErrorResponse {
                 error: "MethodNotAllowed".to_string(),
                 message: "HTTP method not allowed.".to_string(),
