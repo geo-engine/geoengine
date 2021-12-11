4a1ab7f0428c7a74ebfbfec8ee70ebb22f1d0b7e
1lutz <lutzgog@gmail.com>
2021-12-11 12:50:52 +0100

    fix points_bool_csv test

--- operators/src/source/ogr_source.rs
+++ operators/src/source/ogr_source.rs
@@ -4549,7 +4549,11 @@ mod tests {
 
         let pc = MultiPointCollection::from_data(
             MultiPoint::many(vec![vec![(1.1, 2.2)], vec![(3.3, 4.4)], vec![(7.2, 5.9)]]).unwrap(),
-            vec![TimeInterval::default(), TimeInterval::default()],
+            vec![
+                TimeInterval::default(),
+                TimeInterval::default(),
+                TimeInterval::default(),
+            ],
             {
                 let mut map = HashMap::new();
                 map.insert("bool".into(), FeatureData::Bool(vec![true, false, true]));
