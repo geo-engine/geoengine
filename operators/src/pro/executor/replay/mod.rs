b3cce3640346919b237496286858c0fef81403cd
Michael Körber <koerberm@mathematik.uni-marburg.de>
2022-01-19 11:22:46 +0100

    Added docs

--- operators/src/pro/executor/replay/mod.rs
+++ operators/src/pro/executor/replay/mod.rs
@@ -31,48 +31,69 @@ pub enum SubscribeError {
     Closed,
 }
 
-#[allow(clippy::missing_panics_doc)]
+/// Creates a new replay channel with the specified queue size. This means
+/// that the channel holds at most `queue_size` results.
+/// In contrast to, e.g., tokio's broadcast channel, lagging receivers
+/// are dropped or do not miss any result.
+/// However, this means that results can only be processed at the pace
+/// of the slowest receiver.
 pub fn channel<T: Clone + Unpin>(queue_size: usize) -> (Sender<T>, Receiver<T>) {
-    let inner = Arc::new(Mutex::new(Inner::new(queue_size)));
+    let mut inner = Inner::new(queue_size);
 
     let (tx_id, rx_id) = {
-        let mut lock = inner.lock().unwrap();
-        (lock.create_sender(), lock.create_receiver(true))
+        (
+            inner.create_sender(),
+            inner
+                .create_receiver(true)
+                .expect("Initial receiver creation must succeed."),
+        )
     };
+
+    let inner = Arc::new(Mutex::new(inner));
 
     let tx = Sender {
         id: tx_id,
-        pending: Default::default(),
+        pending_futures: Default::default(),
         inner: inner.clone(),
     };
     let rx = Receiver {
-        id: rx_id.unwrap(),
-        pending: Default::default(),
+        id: rx_id,
+        pending_futures: Default::default(),
         inner,
     };
     (tx, rx)
 }
 
-struct BufferEntry<T> {
+/// An entry in the replay queue. Every entry
+/// consists of the actual result and the expected number
+/// of receivers.
+/// This count is decremented every time a receivers consumes
+/// the result. When this count reaches `0`, the entry can
+/// safely be discarded from the queue.
+struct QueueEntry<T> {
     expected_receivers: usize,
     value: T,
 }
 
+/// The actual channel construct.
 struct Inner<T> {
-    consumer_id_seq: u32,
+    receiver_id_seq: u32,
     sender_id_seq: u32,
-    buffer: VecDeque<BufferEntry<T>>,
+    queue: VecDeque<QueueEntry<T>>,
     queue_size: usize,
     offset: usize,
     receivers: HashMap<u32, InnerReceiver>,
     senders: HashMap<u32, InnerSender>,
 }
 
+/// A registered receiver that carries
+/// its next read index.
 struct InnerReceiver {
     idx: usize,
     waker: Option<Waker>,
 }
 
+/// A registered sender
 struct InnerSender {
     waker: Option<Waker>,
 }
@@ -110,11 +131,12 @@ impl<T> Inner<T>
 where
     T: Clone,
 {
+    /// Creates a new Innter instance with the given `queue_size`.
     fn new(queue_size: usize) -> Inner<T> {
         Inner {
-            consumer_id_seq: 0,
+            receiver_id_seq: 0,
             sender_id_seq: 0,
-            buffer: VecDeque::with_capacity(queue_size),
+            queue: VecDeque::with_capacity(queue_size),
             queue_size,
             offset: 0,
             receivers: HashMap::new(),
@@ -122,6 +144,7 @@ where
         }
     }
 
+    /// Creates a new sender and returns its `id`.
     fn create_sender(&mut self) -> u32 {
         let id = self.sender_id_seq;
         self.sender_id_seq += 1;
@@ -133,30 +156,40 @@ where
         id
     }
 
+    /// Removes the sender with the given `id`.
     fn remove_sender(&mut self, id: u32) {
         if self.senders.remove(&id).is_some() && self.senders.is_empty() {
             self.notify_receivers();
         }
     }
 
-    fn clean_up_buffer(&mut self) -> bool {
-        while self.buffer.len() >= self.queue_size
+    /// Tries make space for a new result in the queue.
+    /// This attempt may fail, if the queue is full and
+    /// at least one consumer did not receive the eldest element yet.
+    fn clean_up_queue(&mut self) -> bool {
+        while self.queue.len() >= self.queue_size
             && self
-                .buffer
+                .queue
                 .front()
                 .map_or(false, |e| e.expected_receivers == 0)
         {
-            self.buffer.pop_front().expect("");
+            self.queue.pop_front().expect("");
             self.offset += 1;
         }
-        self.buffer.len() < self.queue_size
+        self.queue.len() < self.queue_size
     }
 
+    /// Tries to send the given value into the channel.
+    ///
+    /// #Errors
+    /// This method fails with [`Closed`](SendError::Closed) if no receivers are left.
+    /// Moreover, [`Full`](SendError::Full) is returned if there is no more space
+    /// within the queue.
     fn try_send(&mut self, value: T) -> Result<(), SendError<T>> {
         if self.receivers.is_empty() {
             Err(SendError::Closed(value))
-        } else if self.clean_up_buffer() {
-            self.buffer.push_back(BufferEntry {
+        } else if self.clean_up_queue() {
+            self.queue.push_back(QueueEntry {
                 expected_receivers: self.receivers.len(),
                 value,
             });
@@ -167,6 +200,7 @@ where
         }
     }
 
+    /// Notifies all pending senders.
     fn notify_senders(&mut self) {
         for v in self.senders.values_mut() {
             if let Some(w) = v.waker.take() {
@@ -175,17 +209,27 @@ where
         }
     }
 
+    /// Updates the waker for the sender with the given `sender_id`.
     fn update_send_waker(&mut self, sender_id: u32, waker: &Waker) {
-        self.senders
-            .get_mut(&sender_id)
-            .unwrap()
-            .update_waker(waker);
+        if let Some(s) = self.senders.get_mut(&sender_id) {
+            s.update_waker(waker);
+        }
     }
 
+    /// Removes the waker for the sender with the given `sender_id`.
     fn remove_send_waker(&mut self, sender_id: u32) {
-        self.senders.get_mut(&sender_id).unwrap().remove_waker();
+        if let Some(s) = self.senders.get_mut(&sender_id) {
+            s.remove_waker();
+        }
     }
 
+    /// Creates a new receivers and returns its id.
+    ///
+    /// # Errors
+    /// This methods fails with [`WouldLag`](SubscribeError::WouldLag), if the first element of
+    /// the result stream was evicted already. Moreover, it returns [`Closed`](SubscribeError::Closed),
+    /// if the channel was closed already (i.e., all senders or receivers were dropped).
+    ///
     fn create_receiver(&mut self, first: bool) -> Result<u32, SubscribeError> {
         if self.offset > 0 {
             return Err(SubscribeError::WouldLag);
@@ -194,12 +238,12 @@ where
         }
 
         // Increment expected read count
-        for v in &mut self.buffer {
+        for v in &mut self.queue {
             v.expected_receivers += 1;
         }
 
-        let id = self.consumer_id_seq;
-        self.consumer_id_seq += 1;
+        let id = self.receiver_id_seq;
+        self.receiver_id_seq += 1;
 
         assert!(self
             .receivers
@@ -214,12 +258,13 @@ where
         Ok(id)
     }
 
+    /// Removes the receiver with the given `id`.
     fn remove_receiver(&mut self, id: u32) {
         if let Some(r) = self.receivers.remove(&id) {
             let idx = r.idx - self.offset;
             let mut notify = false;
-            for i in idx..self.buffer.len() {
-                let e = &mut self.buffer[i];
+            for i in idx..self.queue.len() {
+                let e = &mut self.queue[i];
                 e.expected_receivers -= 1;
                 notify |= e.expected_receivers == 0;
             }
@@ -229,29 +274,37 @@ where
         }
     }
 
+    /// Tries to receive the next message for the receiver with the given `receiver_id`.
+    ///
+    /// # Errors
+    /// Returns [`NoMessage`](ReceiveError::NoMessage) if there is currently no new message available
+    /// for the given receiver - please try again later.  
+    ///
+    /// Moreover, it returns [`Closed`](ReceiveError::Closed) if the channel is closed. No more data
+    /// will be delivered in this case.
     fn try_recv(&mut self, receiver_id: u32) -> Result<T, ReceiveError> {
-        let c = self.receivers.get_mut(&receiver_id).unwrap();
-        let q_idx = c.idx - self.offset;
+        let recv = self
+            .receivers
+            .get_mut(&receiver_id)
+            .expect("Unknown receiver id.");
 
-        let res = match self.buffer.get_mut(q_idx) {
+        let q_idx = recv.idx - self.offset;
+
+        let res = match self.queue.get_mut(q_idx) {
             Some(e) => {
                 e.expected_receivers -= 1;
                 Ok(e.value.clone())
             }
             None if self.senders.is_empty() => Err(ReceiveError::Closed),
             None => Err(ReceiveError::NoMessage),
-        };
+        }?;
 
-        match res {
-            Ok(v) => {
-                c.idx += 1;
-                self.notify_senders();
-                Ok(v)
-            }
-            Err(e) => Err(e),
-        }
+        recv.idx += 1;
+        self.notify_senders();
+        Ok(res)
     }
 
+    /// Notifies all waiting receivers.
     fn notify_receivers(&mut self) {
         for v in self.receivers.values_mut() {
             if let Some(w) = v.waker.take() {
@@ -260,15 +313,17 @@ where
         }
     }
 
-    fn remove_receiver_waker(&mut self, receiver_id: u32) {
-        self.receivers.get_mut(&receiver_id).unwrap().remove_waker();
-    }
-
+    /// Updates the waker for the receiver with the given `receiver_id`.
     fn update_receiver_waker(&mut self, receiver_id: u32, waker: &Waker) {
         self.receivers
             .get_mut(&receiver_id)
             .unwrap()
             .update_waker(waker);
+    }
+
+    /// Removes the waker for the receiver with the given `receiver_id`.
+    fn remove_receiver_waker(&mut self, receiver_id: u32) {
+        self.receivers.get_mut(&receiver_id).unwrap().remove_waker();
     }
 }
 
@@ -278,46 +333,59 @@ where
 //
 // /////////////////////////////////////////////////////////////////////////////
 
+/// The sender side of a replay channel
 pub struct Sender<T>
 where
     T: Clone,
 {
     id: u32,
-    pending: AtomicUsize,
+    pending_futures: AtomicUsize,
     inner: Arc<Mutex<Inner<T>>>,
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Sender<T>
 where
     T: Clone,
 {
+    /// Registers a new receiver to the channel and returns it.
+    ///
+    /// # Errors
+    /// This methods fails with [`WouldLag`](SubscribeError::WouldLag), if the first element of
+    /// the result stream was evicted already. Moreover, it returns [`Closed`](SubscribeError::Closed),
+    /// if the channel was closed already.
     pub fn subscribe(&self) -> Result<Receiver<T>, SubscribeError> {
         let id = {
-            let mut lock = self.inner.lock().unwrap();
+            let mut lock = crate::util::safe_lock_mutex(&self.inner);
             lock.create_receiver(false)
         }?;
 
         Ok(Receiver {
             id,
-            pending: Default::default(),
+            pending_futures: Default::default(),
             inner: self.inner.clone(),
         })
     }
 
+    /// Tries to send the given value immediately. If this is not possible, the
+    /// value is returned with the error.
+    ///
+    /// # Errors
+    /// This method fails with [`Closed`](SendError::Closed) if no receivers are left.
+    /// Moreover, [`Full`](SendError::Full) is returned if there is no more space
+    /// within the queue.
     pub fn try_send(&self, v: T) -> Result<(), SendError<T>> {
-        let mut lock = self.inner.lock().unwrap();
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         lock.try_send(v)
     }
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Sender<T>
 where
     T: Clone + Unpin,
 {
+    /// Sends a value, waiting until there is capacity.
     pub fn send(&self, v: T) -> Send<T> {
-        let _ignore = self.pending.fetch_add(1, Ordering::Relaxed);
+        let _ignore = self.pending_futures.fetch_add(1, Ordering::Relaxed);
         Send {
             sender: self,
             value: Some(v),
@@ -329,8 +397,8 @@ where
         send: &mut Send<'_, T>,
         cx: &mut Context<'_>,
     ) -> Poll<Result<(), SendError<T>>> {
-        let value = send.value.take().unwrap();
-        let mut lock = self.inner.lock().unwrap();
+        let value = send.value.take().expect("Send without message.");
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         match lock.try_send(value) {
             Ok(_) => Poll::Ready(Ok(())),
             Err(SendError::Full(v)) => {
@@ -343,42 +411,41 @@ where
     }
 
     fn send_dropped(&self) {
-        if self.pending.fetch_sub(1, Ordering::Relaxed) == 1 {
-            let mut lock = self.inner.lock().unwrap();
+        if self.pending_futures.fetch_sub(1, Ordering::Relaxed) == 1 {
+            let mut lock = crate::util::safe_lock_mutex(&self.inner);
             lock.remove_send_waker(self.id);
         }
     }
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Clone for Sender<T>
 where
     T: Clone,
 {
     fn clone(&self) -> Self {
         let id = {
-            let mut lock = self.inner.lock().unwrap();
+            let mut lock = crate::util::safe_lock_mutex(&self.inner);
             lock.create_sender()
         };
         Sender {
             id,
-            pending: Default::default(),
+            pending_futures: Default::default(),
             inner: self.inner.clone(),
         }
     }
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Drop for Sender<T>
 where
     T: Clone,
 {
     fn drop(&mut self) {
-        let mut lock = self.inner.lock().unwrap();
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         lock.remove_sender(self.id);
     }
 }
 
+/// A message waiting to be send.
 pub struct Send<'sender, T>
 where
     T: Clone + Unpin,
@@ -413,32 +480,41 @@ where
 //
 // /////////////////////////////////////////////////////////////////////////////
 
+/// The receiver side of a replay channel
 pub struct Receiver<T>
 where
     T: Clone,
 {
     id: u32,
-    pending: AtomicUsize,
+    pending_futures: AtomicUsize,
     inner: Arc<Mutex<Inner<T>>>,
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Receiver<T>
 where
     T: Clone,
 {
+    /// Tries to receive the next message immediately.
+    ///
+    /// # Errors
+    /// Returns [`NoMessage`](ReceiveError::NoMessage) if there is currently no new message available
+    /// - please try again later.  
+    ///
+    /// Moreover, it returns [`Closed`](ReceiveError::Closed) if the channel is closed. No more data
+    /// will be delivered in this case.
     pub fn try_recv(&self) -> Result<T, ReceiveError> {
-        let mut lock = self.inner.lock().unwrap();
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         lock.try_recv(self.id)
     }
 
+    /// Receives the next value from the channel, possibly waiting for it to arrive.
     pub fn recv(&self) -> Recv<T> {
-        let _ignore = self.pending.fetch_add(1, Ordering::Relaxed);
+        let _ignore = self.pending_futures.fetch_add(1, Ordering::Relaxed);
         Recv { receiver: self }
     }
 
     fn poll_recv(&self, cx: &mut Context<'_>) -> Poll<Option<T>> {
-        let mut lock = self.inner.lock().unwrap();
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         match lock.try_recv(self.id) {
             Ok(v) => Poll::Ready(Some(v)),
             Err(ReceiveError::Closed) => Poll::Ready(None),
@@ -450,24 +526,24 @@ where
     }
 
     pub fn recv_dropped(&self) {
-        if self.pending.fetch_sub(1, Ordering::Relaxed) == 1 {
-            let mut lock = self.inner.lock().unwrap();
+        if self.pending_futures.fetch_sub(1, Ordering::Relaxed) == 1 {
+            let mut lock = crate::util::safe_lock_mutex(&self.inner);
             lock.remove_receiver_waker(self.id);
         }
     }
 }
 
-#[allow(clippy::missing_panics_doc)]
 impl<T> Drop for Receiver<T>
 where
     T: Clone,
 {
     fn drop(&mut self) {
-        let mut lock = self.inner.lock().unwrap();
+        let mut lock = crate::util::safe_lock_mutex(&self.inner);
         lock.remove_receiver(self.id);
     }
 }
 
+/// A message waiting to be received.
 pub struct Recv<'receiver, T>
 where
     T: Clone,
@@ -495,6 +571,7 @@ where
     }
 }
 
+/// A stream representation of the `Receiver`.
 pub struct ReceiverStream<T>
 where
     T: Clone,
