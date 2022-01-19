e15e26e77ab4a5899814d361b9d1a0f10ee66194
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2022-01-19 16:55:09 +0100

    test data for netcdf 4d

--- services/src/datasets/external/netcdfcf.rs
+++ services/src/datasets/external/netcdfcf.rs
@@ -128,37 +128,30 @@ impl NetCdfCfDataProvider {
 
         let mut datasets = Vec::new();
         for group_ds in subdataset_iter {
-            // TODO: properly parse the data type from metadata
-            let data_type = if group_ds.desc.contains("32-bit floating-point") {
-                RasterDataType::F32
+            let data_type = if let Some(data_type) = datatype_from_desc(&group_ds.desc) {
+                data_type
             } else {
+                dbg!(group_ds.desc);
                 return Err(Error::NotYetImplemented);
             };
 
-            for entity in 0..num_entities {
+            for entity_idx in 0..num_entities {
+                let group_name = group_ds
+                    .name
+                    .rsplit_once(':')
+                    .ok_or(Error::NetCdfCfMissingMetaData)?
+                    .1;
+                let file_name = path
+                    .file_name()
+                    .ok_or(Error::NetCdfCfMissingMetaData)?
+                    .to_string_lossy();
+
                 datasets.push(DatasetListing {
                     id: DatasetId::External(ExternalDatasetId {
                         provider_id: id,
-                        dataset_id: format!(
-                            "{file_name}::{group_name}::{entity_idx}",
-                            file_name = path
-                                .file_name()
-                                .ok_or(Error::NetCdfCfMissingMetaData)?
-                                .to_string_lossy(),
-                            group_name = group_ds
-                                .name
-                                .rsplit_once(':')
-                                .ok_or(Error::NetCdfCfMissingMetaData)?
-                                .1,
-                            entity_idx = entity
-                        ),
+                        dataset_id: format!("{file_name}::{group_name}::{entity_idx}"),
                     }),
-                    name: format!(
-                        "{title} {group_name} {entity_idx}",
-                        title = title,
-                        group_name = group_ds.name,
-                        entity_idx = entity
-                    ),
+                    name: format!("{title} {file_name} {group_name} {entity_idx}"),
                     description: "".to_owned(), // TODO
                     tags: vec![],               // TODO
                     source_operator: "GdalSource".to_owned(),
@@ -226,7 +219,7 @@ impl NetCdfCfDataProvider {
         let result_descriptor = raster_descriptor_from_dataset(&ds, 1, None)?; // use band 1 because bands are homogeneous
 
         let (start, end, step) = match time_coverage_resolution.as_str() {
-            "Yearly" => {
+            "Yearly" | "every 1 year" => {
                 let start = TimeInstance::from(
                     NaiveDate::from_ymd(time_coverage_start, 1, 1).and_hms(0, 0, 0),
                 );
@@ -237,6 +230,20 @@ impl NetCdfCfDataProvider {
                 let step = TimeStep {
                     granularity: TimeGranularity::Years,
                     step: 1,
+                };
+                (start, end, step)
+            }
+            "decade" => {
+                let start = TimeInstance::from(
+                    NaiveDate::from_ymd(time_coverage_start, 1, 1).and_hms(0, 0, 0),
+                );
+                // end + 1 because it is exclusive for us but inclusive in the metadata
+                let end = TimeInstance::from(
+                    NaiveDate::from_ymd(time_coverage_end + 1, 1, 1).and_hms(0, 0, 0),
+                );
+                let step = TimeStep {
+                    granularity: TimeGranularity::Years,
+                    step: 10,
                 };
                 (start, end, step)
             }
@@ -253,6 +260,32 @@ impl NetCdfCfDataProvider {
             step,
             band_offset: (entity_idx * num_time_steps) as usize,
         }))
+    }
+}
+
+fn strip_datatype_info(desc: &str) -> Option<&str> {
+    if desc.is_empty() {
+        return None;
+    }
+
+    let desc = &desc[..desc.len() - 1];
+
+    Some(desc.rsplit_once('(')?.1)
+}
+
+fn datatype_from_desc(desc: &str) -> Option<RasterDataType> {
+    let desc = strip_datatype_info(desc)?;
+
+    // TODO: add unsigned integers
+
+    match desc {
+        "8-bit integer" => Some(RasterDataType::I8),
+        "16-bit integer" => Some(RasterDataType::I16),
+        "32-bit integer" => Some(RasterDataType::I32),
+        "64-bit integer" => Some(RasterDataType::I64),
+        "32-bit floating-point" => Some(RasterDataType::F32),
+        "64-bit floating-point" => Some(RasterDataType::F64),
+        _ => None,
     }
 }
 
@@ -359,51 +392,334 @@ impl MetaDataProvider<OgrSourceDataset, VectorResultDescriptor, VectorQueryRecta
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
+    use geoengine_datatypes::{
+        primitives::{SpatialPartition2D, SpatialResolution},
+        spatial_reference::SpatialReferenceAuthority,
+        test_data,
+    };
+    use geoengine_operators::source::{
+        FileNotFoundHandling, GdalDatasetGeoTransform, GdalDatasetParameters, GdalLoadingInfoPart,
+    };
 
     use super::*;
 
-    #[test]
-    fn test_name() {
-        let ds = gdal_open_dataset(Path::new(
-            "/home/michael/rust-projects/geoengine/netcdf_data/dataset_sm.nc",
-        ))
+    #[tokio::test]
+    #[allow(clippy::too_many_lines)]
+    async fn test_listing_from_netcdf_m() {
+        let provider_id =
+            DatasetProviderId::from_str("bf6bb6ea-5d5d-467d-bad1-267bf3a54470").unwrap();
+
+        let listing = NetCdfCfDataProvider::listing_from_netcdf(
+            provider_id,
+            test_data!("netcdf4d/dataset_m.nc"),
+        )
         .unwrap();
-        // let meta = ds.metadata_domains();
-        // let meta = dbg!(meta);
 
-        // let global = ds.metadata_domain("");
+        assert_eq!(listing.len(), 6);
 
-        let title = ds.metadata_item("NC_GLOBAL#title", "").unwrap();
-        let time_coverage_start = ds
-            .metadata_item("NC_GLOBAL#time_coverage_start", "")
-            .unwrap();
-        let time_coverage_end = ds.metadata_item("NC_GLOBAL#time_coverage_end", "").unwrap();
-        let time_coverage_resolution = ds
-            .metadata_item("NC_GLOBAL#time_coverage_resolution", "")
-            .unwrap();
-
-        dbg!(title);
-        dbg!(time_coverage_start);
-        dbg!(time_coverage_end);
-        dbg!(time_coverage_resolution);
-
-        let subdatasets = ds.metadata_domain("SUBDATASETS").unwrap();
-
-        for i in (0..subdatasets.len()).step_by(2) {
-            let name = subdatasets[i].split_once('=').unwrap().1;
-            dbg!(&name);
-
-            let ds = gdal_open_dataset(Path::new(&name)).unwrap();
-
-            for band in 1..=ds.raster_count() {
-                let b = ds.rasterband(band).unwrap();
-
-                // let xs = b.x_size();
-                let ys = b.y_size();
-
-                dbg!(ys);
-            }
+        let result_descriptor: TypedResultDescriptor = RasterResultDescriptor {
+            data_type: RasterDataType::I16,
+            spatial_reference: SpatialReference::new(SpatialReferenceAuthority::Epsg, 4326).into(),
+            measurement: Measurement::Unitless,
+            no_data_value: None,
         }
+        .into();
+
+        assert_eq!(
+            listing[0],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_1/ebv_cube::0".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_1/ebv_cube 0".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[1],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_1/ebv_cube::1".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_1/ebv_cube 1".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[2],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_1/ebv_cube::2".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_1/ebv_cube 2".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[3],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_2/ebv_cube::0".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_2/ebv_cube 0".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[4],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_2/ebv_cube::1".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_2/ebv_cube 1".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[5],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_m.nc::/metric_2/ebv_cube::2".into(),
+                }),
+                name: "Test dataset metric dataset_m.nc /metric_2/ebv_cube 2".into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor,
+                symbology: None
+            }
+        );
     }
+
+    #[tokio::test]
+    async fn test_listing_from_netcdf_sm() {
+        let provider_id =
+            DatasetProviderId::from_str("bf6bb6ea-5d5d-467d-bad1-267bf3a54470").unwrap();
+
+        let listing = NetCdfCfDataProvider::listing_from_netcdf(
+            provider_id,
+            test_data!("netcdf4d/dataset_sm.nc"),
+        )
+        .unwrap();
+
+        assert_eq!(listing.len(), 20);
+
+        let result_descriptor: TypedResultDescriptor = RasterResultDescriptor {
+            data_type: RasterDataType::I16,
+            spatial_reference: SpatialReference::new(SpatialReferenceAuthority::Epsg, 3035).into(),
+            measurement: Measurement::Unitless,
+            no_data_value: None,
+        }
+        .into();
+
+        assert_eq!(
+            listing[0],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_sm.nc::/scenario_1/metric_1/ebv_cube::0".into(),
+                }),
+                name:
+                    "Test dataset metric and scenario dataset_sm.nc /scenario_1/metric_1/ebv_cube 0"
+                        .into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor: result_descriptor.clone(),
+                symbology: None
+            }
+        );
+        assert_eq!(
+            listing[19],
+            DatasetListing {
+                id: DatasetId::External(ExternalDatasetId {
+                    provider_id,
+                    dataset_id: "dataset_sm.nc::/scenario_5/metric_2/ebv_cube::1".into(),
+                }),
+                name:
+                    "Test dataset metric and scenario dataset_sm.nc /scenario_5/metric_2/ebv_cube 1"
+                        .into(),
+                description: "".into(),
+                tags: vec![],
+                source_operator: "GdalSource".into(),
+                result_descriptor,
+                symbology: None
+            }
+        );
+    }
+
+    // TODO: verify
+    // TODO: do the samme for `dataset_m.nc`
+    #[tokio::test]
+    async fn test_metadata_from_netcdf_sm() {
+        let provider = NetCdfCfDataProvider {
+            id: DatasetProviderId::from_str("bf6bb6ea-5d5d-467d-bad1-267bf3a54470").unwrap(),
+            path: test_data!("netcdf4d/").to_path_buf(),
+        };
+
+        let metadata = provider
+            .meta_data(&DatasetId::External(ExternalDatasetId {
+                provider_id: provider.id,
+                dataset_id: "dataset_sm.nc::/scenario_5/metric_2/ebv_cube::1".into(),
+            }))
+            .await
+            .unwrap();
+
+        assert_eq!(
+            metadata.result_descriptor().await.unwrap(),
+            RasterResultDescriptor {
+                data_type: RasterDataType::I16,
+                spatial_reference: SpatialReference::new(SpatialReferenceAuthority::Epsg, 3035)
+                    .into(),
+                measurement: Measurement::Unitless,
+                no_data_value: Some(-9999.),
+            }
+        );
+
+        let loading_info = metadata
+            .loading_info(RasterQueryRectangle {
+                spatial_bounds: SpatialPartition2D::new(
+                    (43.945_312_5, 0.791_015_625_25).into(),
+                    (44.033_203_125, 0.703_125_25).into(),
+                )
+                .unwrap(),
+                time_interval: TimeInstance::from(NaiveDate::from_ymd(2001, 4, 1).and_hms(0, 0, 0))
+                    .into(),
+                spatial_resolution: SpatialResolution::new_unchecked(
+                    0.000_343_322_7, // 256 pixel
+                    0.000_343_322_7, // 256 pixel
+                ),
+            })
+            .await
+            .unwrap();
+
+        let mut loading_info_parts = Vec::<GdalLoadingInfoPart>::new();
+        for part in loading_info.info {
+            loading_info_parts.push(part.unwrap());
+        }
+
+        assert_eq!(loading_info_parts.len(), 1);
+
+        let file_path = format!(
+            "NETCDF:{absolute_file_path}:/scenario_5/metric_2/ebv_cube",
+            absolute_file_path = test_data!("netcdf4d/dataset_sm.nc")
+                .canonicalize()
+                .unwrap()
+                .to_string_lossy()
+        )
+        .into();
+
+        assert_eq!(
+            loading_info_parts[0],
+            GdalLoadingInfoPart {
+                time: TimeInterval::new_unchecked(946_684_800_000, 1_262_304_000_000),
+                params: GdalDatasetParameters {
+                    file_path,
+                    rasterband_channel: 4,
+                    geo_transform: GdalDatasetGeoTransform {
+                        origin_coordinate: (3_580_000.0, 2_370_000.0).into(),
+                        x_pixel_size: 1000.0,
+                        y_pixel_size: -1000.0,
+                    },
+                    width: 10,
+                    height: 10,
+                    file_not_found_handling: FileNotFoundHandling::Error,
+                    no_data_value: Some(-9999.),
+                    properties_mapping: None,
+                    gdal_open_options: None,
+                    gdal_config_options: None
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn test_strip_datatype() {
+        assert_eq!(
+            strip_datatype_info("[3x3x5x5] /metric_1/ebv_cube (16-bit integer)"),
+            Some("16-bit integer")
+        );
+    }
+
+    #[test]
+    fn test_datatype_from_desc() {
+        assert_eq!(
+            datatype_from_desc("[3x3x5x5] /metric_1/ebv_cube (16-bit integer)"),
+            Some(RasterDataType::I16)
+        );
+        assert_eq!(
+            datatype_from_desc("[3x3x5x5] /metric_1/ebv_cube (32-bit floating-point)"),
+            Some(RasterDataType::F32)
+        );
+    }
+
+    // #[test]
+    // fn test_name() {
+    //     let ds = gdal_open_dataset(Path::new(
+    //         "/home/michael/rust-projects/geoengine/netcdf_data/dataset_sm.nc",
+    //     ))
+    //     .unwrap();
+    //     // let meta = ds.metadata_domains();
+    //     // let meta = dbg!(meta);
+
+    //     // let global = ds.metadata_domain("");
+
+    //     let title = ds.metadata_item("NC_GLOBAL#title", "").unwrap();
+    //     let time_coverage_start = ds
+    //         .metadata_item("NC_GLOBAL#time_coverage_start", "")
+    //         .unwrap();
+    //     let time_coverage_end = ds.metadata_item("NC_GLOBAL#time_coverage_end", "").unwrap();
+    //     let time_coverage_resolution = ds
+    //         .metadata_item("NC_GLOBAL#time_coverage_resolution", "")
+    //         .unwrap();
+
+    //     dbg!(title);
+    //     dbg!(time_coverage_start);
+    //     dbg!(time_coverage_end);
+    //     dbg!(time_coverage_resolution);
+
+    //     let subdatasets = ds.metadata_domain("SUBDATASETS").unwrap();
+
+    //     for i in (0..subdatasets.len()).step_by(2) {
+    //         let name = subdatasets[i].split_once('=').unwrap().1;
+    //         dbg!(&name);
+
+    //         let ds = gdal_open_dataset(Path::new(&name)).unwrap();
+
+    //         for band in 1..=ds.raster_count() {
+    //             let b = ds.rasterband(band).unwrap();
+
+    //             // let xs = b.x_size();
+    //             let ys = b.y_size();
+
+    //             dbg!(ys);
+    //         }
+    //     }
+    // }
 }
