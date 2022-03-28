686adad8e1f1a69949943d71f11961b7f6b0f6e1
Michael Körber <koerberm@mathematik.uni-marburg.de>
2022-03-28 14:28:24 +0200

    Re-added missing error

--- services/src/error.rs
+++ services/src/error.rs
@@ -212,6 +212,8 @@ pub enum Error {
 
     MissingSpatialReference,
 
+    ExternalAddressNotConfigured,
+
     WcsVersionNotSupported,
     WcsGridOriginMustEqualBoundingboxUpperLeft,
     WcsBoundingboxCrsMustEqualGridBaseCrs,
