d2931b84c4c0ab87267819a4bde9f8147bed727d
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2022-05-19 11:26:50 +0200

    ignore failing doc test

--- operators/src/mock/mock_raster_source.rs
+++ operators/src/mock/mock_raster_source.rs
@@ -171,7 +171,7 @@ impl<T: Pixel> OperatorDatasets for MockRasterSource<T> {
 ///
 /// TODO: implementation is done with `paste!`, but we can use `core::concat_idents` once its stable
 ///
-/// ```compile_fail
+/// ```ignore
 /// #[typetag::serde]
 /// #[async_trait]
 /// impl<T: Pixel> RasterOperator for MockRasterSource<T> {
