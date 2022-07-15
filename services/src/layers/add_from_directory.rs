13019b2b3c7ada2de0a77c9cb3ca124c976ebecc
Michael Mattig <michael.m@ttig.de>
2022-07-15 14:54:35 +0200

    create unsorted layer collection during initialization of db

--- services/src/layers/add_from_directory.rs
+++ services/src/layers/add_from_directory.rs
@@ -49,27 +49,6 @@ pub async fn add_layers_from_directory<L: LayerDb>(layer_db: &mut L, file_path: 
     }
     let dir = dir.expect("checked");
 
-    let unsorted = AddLayerCollection {
-        name: "Unsorted".to_string(),
-        description: "Unsorted Layers".to_string(),
-    }
-    .validated()
-    .expect("unsorted collection is valid");
-
-    let root_id = layer_db
-        .root_collection_id()
-        .await
-        .expect("root id must be resolved");
-
-    layer_db
-        .add_collection_with_id(
-            &LayerCollectionId(UNSORTED_COLLECTION_ID.to_string()),
-            unsorted,
-            &root_id,
-        )
-        .await
-        .expect("unsorted collection should always be added");
-
     for entry in dir {
         match entry {
             Ok(entry) if entry.path().extension() == Some(OsStr::new("json")) => {
