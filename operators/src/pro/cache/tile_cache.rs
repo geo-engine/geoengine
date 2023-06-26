ec74825ab6fe080db6782663ec4ae2274c294c62
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2023-06-26 19:54:22 +0200

    remove todo

--- operators/src/pro/cache/tile_cache.rs
+++ operators/src/pro/cache/tile_cache.rs
@@ -477,7 +477,6 @@ impl TileCache {
             .get_mut(&query_id)
             .ok_or(super::error::CacheError::QueryNotFoundInLandingZone)?;
 
-        // TODO: rollback if one fails?
         entry.query.spatial_bounds = entry.query.spatial_bounds.extend(&tile.spatial_partition()); // since the source should only produce tiles that intersect with the query, we can extend the query bounds
         entry.query.time_interval = entry
             .query
