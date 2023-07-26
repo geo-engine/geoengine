c784b4eff12549df503fc0008b04a6a439f8a238
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2023-07-26 18:23:01 +0200

    benchmark for collection filtering

--- datatypes/benches/multi_point_collection.rs
+++ datatypes/benches/multi_point_collection.rs
@@ -1,6 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use geoengine_datatypes::collections::{
-    BuilderProvider, GeoFeatureCollectionRowBuilder, MultiPointCollection,
+    BuilderProvider, FeatureCollectionInfos, FeatureCollectionModifications,
+    GeoFeatureCollectionRowBuilder, MultiPointCollection,
 };
 use geoengine_datatypes::primitives::{
     Coordinate2D, FeatureDataType, FeatureDataValue, TimeInterval,
@@ -38,6 +39,68 @@ fn multi_point_collection_benchmarks(c: &mut Criterion) {
             }
             black_box(builder.build())
         })
+    });
+
+    group.bench_function("Filter multi point chunk", |b| {
+        b.iter_batched(
+            || {
+                let mut builder = MultiPointCollection::builder();
+                builder
+                    .add_column("float".into(), FeatureDataType::Float)
+                    .unwrap();
+                builder
+                    .add_column("int".into(), FeatureDataType::Int)
+                    .unwrap();
+                builder
+                    .add_column("text".into(), FeatureDataType::Text)
+                    .unwrap();
+                let mut builder = builder.finish_header();
+                for i in 0.. {
+                    builder.push_geometry(Coordinate2D::new(i as f64, i as f64).into());
+                    builder.push_time_interval(TimeInterval::new_unchecked(i, i + 1));
+
+                    // add some nulls
+                    if i % 10 == 0 {
+                        builder.push_null("float").unwrap();
+                        builder.push_null("int").unwrap();
+                        builder.push_null("text").unwrap();
+                    } else {
+                        builder
+                            .push_data("float", FeatureDataValue::Float(i as f64))
+                            .unwrap();
+                        builder.push_data("int", FeatureDataValue::Int(i)).unwrap();
+                        builder
+                            .push_data("text", FeatureDataValue::Text(i.to_string()))
+                            .unwrap();
+                    }
+
+                    builder.finish_row();
+
+                    // from settings-default.toml
+                    if builder.estimate_memory_size() >= 1048576 {
+                        break;
+                    }
+                }
+                builder.build().unwrap()
+            },
+            |data| {
+                let third = data.len() / 3;
+
+                data.column_range_filter(
+                    "float",
+                    &[
+                        FeatureDataValue::Float(0.)..=FeatureDataValue::Float(third as f64),
+                        FeatureDataValue::Float(2. * third as f64)
+                            ..=FeatureDataValue::Float(3. * third as f64),
+                    ],
+                    true,
+                )
+                .unwrap();
+
+                black_box(data)
+            },
+            criterion::BatchSize::SmallInput,
+        )
     });
 
     group.finish();
