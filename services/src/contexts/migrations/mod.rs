ec3985d6675b596ec00ca51b9a7c09c2386d906f
Dominik Brandenstein <dominik.brandenstein@uni-marburg.de>
2023-12-19 13:57:00 +0100

    Register new database migration

--- services/src/contexts/migrations/mod.rs
+++ services/src/contexts/migrations/mod.rs
@@ -5,8 +5,9 @@ use self::migration_0000_initial::Migration0000Initial;
 mod database_migration;
 pub mod migration_0000_initial;
 pub mod migration_0001_raster_stacks;
-mod migration_0002_gbif_config;
+pub mod migration_0002_gbif_config;
 
+use crate::contexts::migrations::migration_0002_gbif_config::Migration0002GbifConfig;
 pub use database_migration::{migrate_database, DatabaseVersion, Migration, MigrationResult};
 
 /// All migrations that are available. The migrations are applied in the order they are defined here, starting from the current version of the database.
@@ -17,5 +18,6 @@ pub fn all_migrations() -> Vec<Box<dyn Migration>> {
     vec![
         Box::new(Migration0000Initial),
         Box::new(Migration0001RasterStacks),
+        Box::new(Migration0002GbifConfig),
     ]
 }
