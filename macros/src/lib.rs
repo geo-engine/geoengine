70935f519bd58e62b128e7b4e7a37ca2b82bd0f2
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2024-01-23 16:11:56 +0100

    lints in macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,33 @@
+// configure default clippy lints
+#![deny(clippy::correctness)]
+#![warn(clippy::complexity, clippy::style, clippy::perf, clippy::pedantic)]
+// disable some pedantic lints
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::default_trait_access,
+    clippy::missing_errors_doc,
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::non_ascii_literal,
+    clippy::option_if_let_else,
+    clippy::result_large_err, // TODO: investigate this
+    clippy::similar_names,
+    clippy::single_match_else,
+    clippy::type_repetition_in_bounds,
+    clippy::wildcard_imports
+)]
+// enable some restriction lints
+#![warn(
+    clippy::dbg_macro,
+    clippy::print_stderr,
+    clippy::print_stdout,
+    clippy::unimplemented,
+    clippy::unwrap_used
+)]
+
 use proc_macro2::TokenStream;
 
 /// Compiles Geo Engine Pro
@@ -21,7 +51,7 @@ pub fn test(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    match testing::test(attr.into(), item.clone().into()) {
+    match testing::test(attr.into(), &item.clone().into()) {
         Ok(ts) => ts.into(),
         Err(e) => token_stream_with_error(item.into(), e).into(),
     }
@@ -51,7 +81,7 @@ pub fn pro_test(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    match pro::test(attr.into(), item.clone().into()) {
+    match pro::test(attr.into(), &item.clone().into()) {
         Ok(ts) => ts.into(),
         Err(e) => token_stream_with_error(item.into(), e).into(),
     }
