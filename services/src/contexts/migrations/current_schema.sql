332ecd840e42cc59594e747b4c237b1bda5a3ce7
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2024-03-13 11:45:49 +0100

    incorporate latest schema change

--- services/src/contexts/migrations/current_schema.sql
+++ services/src/contexts/migrations/current_schema.sql
@@ -708,8 +708,7 @@ CREATE TYPE "EbvPortalDataProviderDefinition" AS (
     overviews text,
     cache_ttl int,
     description text,
-    priority smallint,
-    metadata_db_config "DatabaseConnectionConfig"
+    priority smallint
 );
 
 CREATE TYPE "NetCdfCfDataProviderDefinition" AS (
@@ -718,8 +717,7 @@ CREATE TYPE "NetCdfCfDataProviderDefinition" AS (
     overviews text,
     cache_ttl int,
     description text,
-    priority smallint,
-    metadata_db_config "DatabaseConnectionConfig"
+    priority smallint
 );
 
 CREATE TYPE "PangaeaDataProviderDefinition" AS (
@@ -786,3 +784,92 @@ CREATE TABLE layer_providers (
 );
 
 -- TODO: relationship between uploads and datasets?
+
+-- EBV PROVIDER TABLE DEFINITIONS
+
+CREATE TABLE ebv_provider_dataset_locks (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name)
+);
+
+CREATE TABLE ebv_provider_overviews (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+    title text NOT NULL,
+    summary text NOT NULL,
+    spatial_reference "SpatialReference" NOT NULL,
+    colorizer "Colorizer" NOT NULL,
+    creator_name text,
+    creator_email text,
+    creator_institution text,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name)
+);
+
+CREATE TABLE ebv_provider_groups (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+    name text [] NOT NULL,
+    title text NOT NULL,
+    description text NOT NULL,
+    data_type "RasterDataType",
+    data_range float [2],
+    unit text NOT NULL,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name, name) DEFERRABLE,
+
+    FOREIGN KEY (provider_id, file_name) REFERENCES ebv_provider_overviews (
+        provider_id,
+        file_name
+    ) ON DELETE CASCADE DEFERRABLE
+);
+
+CREATE TABLE ebv_provider_entities (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+    id bigint NOT NULL,
+    name text NOT NULL,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name, id) DEFERRABLE,
+
+    FOREIGN KEY (provider_id, file_name) REFERENCES ebv_provider_overviews (
+        provider_id,
+        file_name
+    ) ON DELETE CASCADE DEFERRABLE
+);
+
+CREATE TABLE ebv_provider_timestamps (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+    "time" bigint NOT NULL,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name, "time") DEFERRABLE,
+
+    FOREIGN KEY (provider_id, file_name) REFERENCES ebv_provider_overviews (
+        provider_id,
+        file_name
+    ) ON DELETE CASCADE DEFERRABLE
+);
+
+CREATE TABLE ebv_provider_loading_infos (
+    provider_id uuid NOT NULL,
+    file_name text NOT NULL,
+    group_names text [] NOT NULL,
+    entity_id bigint NOT NULL,
+    meta_data "GdalMetaDataList" NOT NULL,
+
+    -- TODO: check if we need it
+    PRIMARY KEY (provider_id, file_name, group_names, entity_id) DEFERRABLE,
+
+    FOREIGN KEY (provider_id, file_name) REFERENCES ebv_provider_overviews (
+        provider_id,
+        file_name
+    ) ON DELETE CASCADE DEFERRABLE
+);
