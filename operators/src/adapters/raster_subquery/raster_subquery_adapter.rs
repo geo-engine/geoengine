7ce031e60e1c441b388996d5680d454b388c15ee
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2024-04-15 14:59:54 +0200

    remove assert thats not always true

--- operators/src/adapters/raster_subquery/raster_subquery_adapter.rs
+++ operators/src/adapters/raster_subquery/raster_subquery_adapter.rs
@@ -131,13 +131,6 @@ where
         query_ctx: &'a dyn QueryContext,
         sub_query: SubQuery,
     ) -> Self {
-        debug_assert!(
-            source_processor
-                .raster_result_descriptor()
-                .tiling_geo_transform()
-                == tiling_strategy.geo_transform
-        );
-
         let grid_bounds = query_rect_to_answer.spatial_query.grid_bounds();
         let tile_bounds = tiling_strategy.global_pixel_grid_bounds_to_tile_grid_bounds(grid_bounds);
 
