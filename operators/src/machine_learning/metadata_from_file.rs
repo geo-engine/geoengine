da45ed2464c0db44f5ba8c68d7b9f56fac459130
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2024-12-06 15:25:01 +0100

    update deps 2024-12-06

--- operators/src/machine_learning/metadata_from_file.rs
+++ operators/src/machine_learning/metadata_from_file.rs
@@ -8,7 +8,7 @@ use std::path::Path;
 
 pub fn load_model_metadata(path: &Path) -> Result<MlModelMetadata, MachineLearningError> {
     // TODO: proper error if model file cannot be found
-    let session = ort::Session::builder()
+    let session = ort::session::Session::builder()
         .context(Ort)?
         .commit_from_file(path)
         .context(Ort)?;
@@ -22,9 +22,10 @@ pub fn load_model_metadata(path: &Path) -> Result<MlModelMetadata, MachineLearni
     );
 
     // Onnx model input type must be a Tensor in order to accept a 2d ndarray as input
-    let ort::ValueType::Tensor {
+    let ort::value::ValueType::Tensor {
         ty: input_tensor_element_type,
         dimensions: input_dimensions,
+        dimension_symbols: _dimension_symbols,
     } = &session.inputs[0].input_type
     else {
         return Err(MachineLearningError::InvalidInputType {
@@ -43,21 +44,25 @@ pub fn load_model_metadata(path: &Path) -> Result<MlModelMetadata, MachineLearni
     // Onnx model must output one prediction per pixel as
     // (1) a Tensor with a single dimension of unknown size (dim = [-1]), or
     // (2) a Tensor with two dimensions, the first of unknown size and the second of size 1 (dim = [-1, 1])
-    let output_tensor_element_type =
-        if let ort::ValueType::Tensor { ty, dimensions } = &session.outputs[0].output_type {
-            ensure!(
-                dimensions == &[-1] || dimensions == &[-1, 1],
-                InvalidOutputDimensions {
-                    dimensions: dimensions.clone()
-                }
-            );
+    let output_tensor_element_type = if let ort::value::ValueType::Tensor {
+        ty,
+        dimensions,
+        dimension_symbols: _dimension_symbols,
+    } = &session.outputs[0].output_type
+    {
+        ensure!(
+            dimensions == &[-1] || dimensions == &[-1, 1],
+            InvalidOutputDimensions {
+                dimensions: dimensions.clone()
+            }
+        );
 
-            ty
-        } else {
-            return Err(MachineLearningError::InvalidOutputType {
-                output_type: session.outputs[0].output_type.clone(),
-            });
-        };
+        ty
+    } else {
+        return Err(MachineLearningError::InvalidOutputType {
+            output_type: session.outputs[0].output_type.clone(),
+        });
+    };
 
     Ok(MlModelMetadata {
         file_path: path.to_owned(),
@@ -69,19 +74,21 @@ pub fn load_model_metadata(path: &Path) -> Result<MlModelMetadata, MachineLearni
 
 // can't implement `TryFrom` here because `RasterDataType` is in operators crate
 fn try_raster_datatype_from_tensor_element_type(
-    value: ort::TensorElementType,
+    value: ort::tensor::TensorElementType,
 ) -> Result<RasterDataType, MachineLearningError> {
     match value {
-        ort::TensorElementType::Float32 => Ok(RasterDataType::F32),
-        ort::TensorElementType::Uint8 | ort::TensorElementType::Bool => Ok(RasterDataType::U8),
-        ort::TensorElementType::Int8 => Ok(RasterDataType::I8),
-        ort::TensorElementType::Uint16 => Ok(RasterDataType::U16),
-        ort::TensorElementType::Int16 => Ok(RasterDataType::I16),
-        ort::TensorElementType::Int32 => Ok(RasterDataType::I32),
-        ort::TensorElementType::Int64 => Ok(RasterDataType::I64),
-        ort::TensorElementType::Float64 => Ok(RasterDataType::F64),
-        ort::TensorElementType::Uint32 => Ok(RasterDataType::U32),
-        ort::TensorElementType::Uint64 => Ok(RasterDataType::U64),
+        ort::tensor::TensorElementType::Float32 => Ok(RasterDataType::F32),
+        ort::tensor::TensorElementType::Uint8 | ort::tensor::TensorElementType::Bool => {
+            Ok(RasterDataType::U8)
+        }
+        ort::tensor::TensorElementType::Int8 => Ok(RasterDataType::I8),
+        ort::tensor::TensorElementType::Uint16 => Ok(RasterDataType::U16),
+        ort::tensor::TensorElementType::Int16 => Ok(RasterDataType::I16),
+        ort::tensor::TensorElementType::Int32 => Ok(RasterDataType::I32),
+        ort::tensor::TensorElementType::Int64 => Ok(RasterDataType::I64),
+        ort::tensor::TensorElementType::Float64 => Ok(RasterDataType::F64),
+        ort::tensor::TensorElementType::Uint32 => Ok(RasterDataType::U32),
+        ort::tensor::TensorElementType::Uint64 => Ok(RasterDataType::U64),
         _ => Err(MachineLearningError::UnsupportedTensorElementType {
             element_type: value,
         }),
