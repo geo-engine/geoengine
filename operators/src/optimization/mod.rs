8c1bd2d3a02dd0b9b13e8d1f12289ae0fc2cbd27
Michael Mattig <michael.m@ttig.de>
2025-02-05 17:01:33 +0100

    add downscaled ndvi test data set

--- operators/src/optimization/mod.rs
+++ operators/src/optimization/mod.rs
@@ -25,11 +25,21 @@ pub fn resolution_compatible_for_downsampling(
 
 #[cfg(test)]
 mod tests {
-    use geoengine_datatypes::{raster::RasterDataType, util::test::TestDefault};
+    use geoengine_datatypes::{
+        dataset::{DataId, DatasetId, NamedData},
+        raster::{GeoTransform, GridBoundingBox2D, RasterDataType, RasterTile2D, RenameBands},
+        spatial_reference::SpatialReference,
+        util::test::TestDefault,
+    };
 
     use crate::{
-        engine::{MockExecutionContext, RasterOperator, SingleRasterSource, WorkflowOperatorPath},
-        processing::{Expression, ExpressionParams},
+        engine::{
+            MockExecutionContext, MultipleRasterSources, RasterBandDescriptors, RasterOperator,
+            RasterResultDescriptor, SingleRasterSource, SpatialGridDescriptor,
+            WorkflowOperatorPath,
+        },
+        mock::{MockRasterSource, MockRasterSourceParams},
+        processing::{Expression, ExpressionParams, RasterStacker, RasterStackerParams},
         source::{GdalSource, GdalSourceParameters},
         util::gdal::add_ndvi_dataset,
     };
@@ -207,4 +217,118 @@ mod tests {
             SpatialResolution::new(1., 1.).unwrap()
         );
     }
+
+    fn mock_raster_source(resolution: SpatialResolution) -> Box<dyn RasterOperator> {
+        let result_descriptor = RasterResultDescriptor {
+            data_type: RasterDataType::U8,
+            spatial_reference: SpatialReference::epsg_4326().into(),
+            time: None,
+            spatial_grid: SpatialGridDescriptor::source_from_parts(
+                GeoTransform::new_with_coordinate_x_y(0.0, resolution.x, 0.0, -resolution.y),
+                GridBoundingBox2D::new([-2, 0], [0, 4]).unwrap(),
+            ),
+            bands: RasterBandDescriptors::new_single_band(),
+        };
+
+        let data: Vec<RasterTile2D<u8>> = vec![];
+
+        MockRasterSource {
+            params: MockRasterSourceParams {
+                data,
+                result_descriptor,
+            },
+        }
+        .boxed()
+    }
+
+    // #[tokio::test]
+    // async fn it_removes_upsampling() {
+    //     let mut exe_ctx = MockExecutionContext::test_default();
+
+    //     let id: DataId = DatasetId::new().into();
+    //     exe_ctx.add_meta_data(
+    //         id.clone(),
+    //         NamedData::with_system_name("data0"),
+    //         Box::new(MockDatasetDataSourceLoadingInfo {
+    //             points: vec![Coordinate2D::new(1., 2.); 3],
+    //         }),
+    //     );
+
+    //     let mps = MockDatasetDataSource {
+    //         params: MockDatasetDataSourceParams {
+    //             data: NamedData::with_system_name("points"),
+    //         },
+    //     }
+    //     .boxed();
+
+    //     let stacker = RasterStacker {
+    //         params: RasterStackerParams {
+    //             rename_bands: RenameBands::Default,
+    //         },
+    //         sources: MultipleRasterSources {
+    //             rasters: vec![
+    //                 mock_raster_source(SpatialResolution::new_unchecked(0.1, 0.1)),
+    //                 mock_raster_source(SpatialResolution::new_unchecked(0.1, 0.1)),
+    //             ],
+    //         },
+    //     }
+    //     .boxed();
+
+    //     let gdal_initialized = gdal
+    //         .initialize(WorkflowOperatorPath::initialize_root(), &exe_ctx)
+    //         .await
+    //         .unwrap();
+
+    //     assert_eq!(
+    //         gdal_initialized
+    //             .result_descriptor()
+    //             .spatial_grid
+    //             .spatial_resolution(),
+    //         SpatialResolution::new(0.1, 0.1).unwrap()
+    //     );
+
+    //     let gdal_optimized = gdal_initialized
+    //         .optimize(SpatialResolution::new(1., 1.).unwrap())
+    //         .unwrap();
+
+    //     let json = serde_json::to_value(&gdal_optimized).unwrap();
+
+    //     assert_eq!(
+    //         json,
+    //         serde_json::json!({
+    //             "params": {
+    //                 "outputOriginReference": null,
+    //                 "outputResolution": {
+    //                     "type": "resolution",
+    //                     "x": 1.0,
+    //                     "y": 1.0
+    //                 },
+    //                 "samplingMethod": "nearestNeighbor"
+    //             },
+    //             "sources": {
+    //                 "raster": {
+    //                     "params": {
+    //                         "data": "ndvi",
+    //                         "overviewLevel": 3
+    //                     },
+    //                     "type": "GdalSource"
+    //                 }
+    //             },
+    //             "type": "Downsampling"
+    //         })
+    //     );
+
+    //     let gdal_optimized_initialized = gdal_optimized
+    //         .initialize(WorkflowOperatorPath::initialize_root(), &exe_ctx)
+    //         .await
+    //         .unwrap();
+
+    //     assert_eq!(
+    //         gdal_optimized_initialized
+    //             .result_descriptor()
+    //             .spatial_grid
+    //             .spatial_resolution(),
+    //         SpatialResolution::new(1., 1.).unwrap()
+    //     );
+    // }
 }
