fc5f09733cd2ed3f7db4c5c96b3a1cced724c9bf
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-02-20 10:13:23 +0100

    postgres in CI container

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-echo "[Checking with Rustfmt]"
+function print_headline() {
+    local BOLD_WHITE_ON_CYAN="\e[1;46;37m"
+    local BOLD_CYAN="\e[1;49;36m"
+    local RESET_COLOR="\e[0m"
+    printf "${BOLD_WHITE_ON_CYAN} ▶ ${BOLD_CYAN} $1 ${RESET_COLOR}\n" >&2
+}
+
+print_headline "Checking with Rustfmt"
 cargo fmt --all -- --check
 
-echo "[Checking with Clippy]"
+print_headline "Checking with Clippy"
 cargo clippy --all-targets --locked -- -D warnings
 
-echo "[Checking with SQLFluff]"
+print_headline "Checking with SQLFluff"
 pipx run sqlfluff==3.3.0 lint
 
-echo "[Verifying expression dependencies workspace]"
+print_headline "Verifying expression dependencies workspace"
 rustup toolchain install nightly
 chmod +x ./.scripts/check-expression-deps.rs
 ./.scripts/check-expression-deps.rs
 
-echo "[Running tests]"
+print_headline "Running tests"
 cargo test --locked --verbose
