cf24d28b5dfe0ca368e7079fca4dc494f69ee355
Dominik Brandenstein <dominik.brandenstein@uni-marburg.de>
2025-08-15 15:31:12 +0200

    Sqlfluff

--- services/src/contexts/migrations/migration_0021_default_permissions_for_existing_providers.sql
+++ services/src/contexts/migrations/migration_0021_default_permissions_for_existing_providers.sql
@@ -7,33 +7,33 @@ INSERT INTO permissions (role_id, permission, provider_id)
 SELECT
     'd5328854-6190-4af9-ad69-4e74b0961ac9' AS role_id,
     'Owner' AS permission,
-    id AS provider_id
-FROM layer_providers
+    p.id AS provider_id
+FROM layer_providers AS p
 WHERE NOT EXISTS (
-    SELECT skip_id FROM skip
-    WHERE id = skip_id
+    SELECT s.skip_id FROM skip AS s
+    WHERE p.id = s.skip_id
 );
 
 INSERT INTO permissions (role_id, permission, provider_id)
 SELECT
     '4e8081b6-8aa6-4275-af0c-2fa2da557d28' AS role_id,
     'Read' AS permission,
-    id AS provider_id
-FROM layer_providers
+    p.id AS provider_id
+FROM layer_providers AS p
 WHERE NOT EXISTS (
-    SELECT skip_id FROM skip
-    WHERE id = skip_id
+    SELECT s.skip_id FROM skip AS s
+    WHERE p.id = s.skip_id
 );
 
 INSERT INTO permissions (role_id, permission, provider_id)
 SELECT
     'fd8e87bf-515c-4f36-8da6-1a53702ff102' AS role_id,
     'Read' AS permission,
-    id AS provider_id
-FROM layer_providers
+    p.id AS provider_id
+FROM layer_providers AS p
 WHERE NOT EXISTS (
-    SELECT skip_id FROM skip
-    WHERE id = skip_id
+    SELECT s.skip_id FROM skip AS s
+    WHERE p.id = s.skip_id
 );
 
 DROP TABLE skip;
