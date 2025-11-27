8de4ad7e8efa267b528d71caa74b90aac2b43403
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-11-27 13:31:36 +0100

    fix: use fewer rustup deps for expression (#1098)
    * fix: use fewer rustup deps for expression
    
    * expression toolchain file in CLI

--- expression/src/util.rs
+++ expression/src/util.rs
@@ -1,4 +1,7 @@
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    io::{BufWriter, Write},
+};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum DuplicateOrEmpty {
@@ -24,4 +27,53 @@ pub fn duplicate_or_empty_str_slice<S: AsRef<str>>(strings: &[S]) -> DuplicateOr
     }
 
     DuplicateOrEmpty::Ok
+}
+
+/// Writes a minimal rustup toolchain file based on the toolchain file used by the geoengine crate
+pub fn write_minimal_toolchain_file(
+    writer: &mut BufWriter<impl Write + Unpin>,
+    rust_toolchain_toml: &str,
+) -> Result<(), std::io::Error> {
+    const COMPONENTS_LINE_PREFIX: &str = "components = ";
+    const REPLACEMENT_COMPONENTS: &str = r#"["rustc", "cargo", "rust-std"]"#;
+
+    for line in rust_toolchain_toml.lines() {
+        if line.starts_with(COMPONENTS_LINE_PREFIX) {
+            writer.write_all(COMPONENTS_LINE_PREFIX.as_bytes())?;
+            writer.write_all(REPLACEMENT_COMPONENTS.as_bytes())?;
+        } else {
+            writer.write_all(line.as_bytes())?;
+        }
+        writer.write_all(b"\n")?;
+    }
+
+    writer.flush()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn it_write_minimal_toolchain_file() {
+        const INPUT: &str = indoc! {r#"
+        [toolchain]
+        channel = "1.91.1"
+        components = ["cargo", "rustfmt", "rust-src", "clippy", "llvm-tools"]
+        "#};
+        const EXPECTED_OUTPUT: &str = indoc! {r#"
+        [toolchain]
+        channel = "1.91.1"
+        components = ["rustc", "cargo", "rust-std"]
+        "#};
+
+        let mut output = Vec::new();
+        {
+            let mut writer = BufWriter::new(&mut output);
+            write_minimal_toolchain_file(&mut writer, INPUT).unwrap();
+        }
+        let output_str = String::from_utf8(output).unwrap();
+        assert_eq!(output_str, EXPECTED_OUTPUT);
+    }
 }
