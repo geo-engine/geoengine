d958cbf092104dcad6cf9e88dce370dfe1e10585
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2025-12-16 12:07:01 +0100

    more helpful debug output

--- operators/src/engine/query_processor.rs
+++ operators/src/engine/query_processor.rs
@@ -137,8 +137,13 @@ pub trait RasterQueryProcessor:
     ) -> Result<BoxStream<'a, Result<TimeInterval>>> {
         let rdt = self.raster_result_descriptor().time;
         if let Some(regular_time) = rdt.dimension.unwrap_regular() {
-            debug!("Using time query shortcut for regular time dimension");
-            let iter = regular_time.intersecting_intervals(query)?.map(Result::Ok);
+            debug!(
+                "Using time query shortcut for regular time dimension: {regular_time:?} and query: {query:?}"
+            );
+            let iter = regular_time
+                .intersecting_intervals(query)?
+                .inspect(|t| debug!("time_query regular shortcut yielded {t:?}"))
+                .map(Result::Ok);
             return Ok(futures::StreamExt::boxed(futures::stream::iter(iter)));
         }
 
