eedf0265fd053213c4718e927d037e690f41677a
Dominik Brandenstein <dominik.brandenstein@uni-marburg.de>
2026-05-21 14:47:10 +0200

    v0.9.2

--- api-clients/python/geoengine_api_client/configuration.py
+++ api-clients/python/geoengine_api_client/configuration.py
@@ -513,8 +513,8 @@ class Configuration:
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 0.9.1\n"\
-               "SDK Package Version: 0.9.1".\
+               "Version of the API: 0.9.2\n"\
+               "SDK Package Version: 0.9.2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
