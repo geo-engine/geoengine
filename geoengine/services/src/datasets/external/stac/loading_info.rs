e463f82e503da6918ac15b40f004315ef7aca3c0
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2026-07-06 23:48:42 +0200

    change stac lookup coordinates smaller number of digits

--- geoengine/services/src/datasets/external/stac/loading_info.rs
+++ geoengine/services/src/datasets/external/stac/loading_info.rs
@@ -1,6 +1,7 @@
 use super::common;
 use super::{StacDataProvider, StacProviderDataset, StacProviderS3Config, cache::StacQueryCache};
 use crate::error::Result;
+use crate::util::format_stac_wgs84_bbox;
 use crate::util::join_base_url_and_path;
 use crate::util::retry::{RetryPolicy, retry_http};
 use async_trait::async_trait;
@@ -370,13 +371,7 @@ impl StacMultiBandMetaData {
         let query_params = vec![
             (
                 "bbox".to_owned(),
-                format!(
-                    "{},{},{},{}",
-                    bbox.lower_left().x,
-                    bbox.lower_left().y,
-                    bbox.upper_right().x,
-                    bbox.upper_right().y
-                ),
+                format_stac_wgs84_bbox(bbox),
             ),
             (
                 "datetime".to_owned(),
