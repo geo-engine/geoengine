c1df66ea85f1dfb9f1751b9ad8ce25b3e7fe79b5
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2026-07-27 10:56:42 +0200

    fix: update TypedResultDescriptor to use untagged serde representation

--- api-clients/.generation/post-process/rust.py
+++ api-clients/.generation/post-process/rust.py
@@ -31,6 +31,7 @@ def file_modifications() -> Generator[tuple[Path, FileModifier], None, None]:
         Path("src/models/meta_data_definition.rs"),
         Path("src/models/raster_operator.rs"),
         Path("src/models/spatial_bounds_derive.rs"),
+        Path("src/models/typed_result_descriptor.rs"),
         Path("src/models/vector_operator.rs"),
     ]:
         yield file_path, make_untagged
