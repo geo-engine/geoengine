269595e281b57be9c73d9a4c8a5c1d301c54a4a7
Michael Mattig <michael.m@ttig.de>
2026-09-21 15:25:55 +0000

    lint

--- geoengine/services/src/contexts/migrations/migration_0031_stac_provider_authentication.sql
+++ geoengine/services/src/contexts/migrations/migration_0031_stac_provider_authentication.sql
@@ -1,4 +1,4 @@
--- Preserve existing plaintext credentials while replacing their text attributes.
+-- Preserve existing plaintext credentials while replacing text attributes.
 ALTER TYPE "StacProviderS3Config"
 RENAME ATTRIBUTE access_key TO legacy_access_key;
 ALTER TYPE "StacProviderS3Config"
@@ -9,14 +9,21 @@ ALTER TYPE "StacProviderS3Config" ADD ATTRIBUTE secret_key bytea;
 UPDATE layer_providers
 SET
     definition.stac_data_provider_definition.s3_config.access_key = convert_to(
-        (((definition).stac_data_provider_definition).s3_config).legacy_access_key,
+        (
+            ((definition).stac_data_provider_definition).s3_config
+        ).legacy_access_key,
         'UTF8'
     ),
     definition.stac_data_provider_definition.s3_config.secret_key = convert_to(
-        (((definition).stac_data_provider_definition).s3_config).legacy_secret_key,
+        (
+            ((definition).stac_data_provider_definition).s3_config
+        ).legacy_secret_key,
         'UTF8'
     )
-WHERE ((definition).stac_data_provider_definition).s3_config IS DISTINCT FROM NULL;
+WHERE
+    (
+        (definition).stac_data_provider_definition
+    ).s3_config IS DISTINCT FROM NULL;
 
 ALTER TYPE "StacProviderS3Config" DROP ATTRIBUTE legacy_access_key;
 ALTER TYPE "StacProviderS3Config" DROP ATTRIBUTE legacy_secret_key;
